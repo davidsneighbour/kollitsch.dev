e33b230e378d8e2a01bbe887468f91b3756ac01a
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-09-29 19:45:10 +0700

    chore: add dev server scripts

---
+++ bin/hugo-dev-server.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+REQUIRED_TOOLS=(
+  hugo
+  npm
+)
+
+for TOOL in "${REQUIRED_TOOLS[@]}"; do
+  if ! command -v "${TOOL}" >/dev/null; then
+    echo "${TOOL} is required... "
+    exit 1
+  fi
+done
+
+# cleanup hugo loggin
+npm run clean:hugo
+
+# starting hugo server
+hugo server \
+    --gc \
+    --environment development \
+    --disableFastRender \
+    --i18n-warnings \
+    --navigateToChanged \
+    --templateMetrics \
+    --templateMetricsHints \
+    --path-warnings \
+    --poll 1s \
+    --cleanDestinationDir \
+    --renderToDisk \
+    --buildDrafts --buildExpired --buildFuture \
+    --watch \
+    --enableGitInfo \
+    --forceSyncStatic \
+    --log true --logFile hugo.log \
+    --verbose \
+    --verboseLog \
+    --port 1313 \
+    --baseURL http://localhost/
+    #--bind 192.168.1.201 \

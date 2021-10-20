9be05a4190e562af822ccb02ad8d0473dcd2d6e5
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2021-10-20 16:57:04 +0700

    chore(config): ignore compiled javascript files in assets folder

--- assets/js/components/preloader.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.preloader = void 0;
-function preloader(term) {
-    if (term === void 0) { term = 'bla'; }
-    var bla = term + "1";
-    alert(bla);
-}
-exports.preloader = preloader;

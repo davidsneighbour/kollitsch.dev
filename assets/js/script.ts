2fbabc53e6a1e83f713def99d7614c2dd7d892bd
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2021-10-29 19:42:35 +0700

    theme(fix): remove file extension from import in assets/js

--- assets/js/script.ts
+++ assets/js/script.ts
@@ -1,8 +1,10 @@
 import Alpine from 'alpinejs';
-import { preloader } from './components/preloader.js';
+import { preloader } from './components/preloader';
+import { Alert, Button, Carousel, Collapse, Dropdown, Modal, Offcanvas, Popover, ScrollSpy, Tab, Toast, Tooltip } from 'bootstrap';
 
 // preloader
 preloader("GNAH");
 
 // alpine
 Alpine.start()
+

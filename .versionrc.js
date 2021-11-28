d72c5bc64b4e31484563b3ff71c4738e4f6688d5
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-11-28 19:10:12 +0700

    chore(config): fix pwa implementation

--- .versionrc.js
+++ .versionrc.js
@@ -1,10 +1,10 @@
-const defaultStandardVersion = require("@dnb-org/standard-version-config");
-const localStandardVersion =  {
+const defaultStandardVersion = require('@dnb-org/standard-version-config');
+const localStandardVersion = {
   bumpFiles: [
     ...defaultStandardVersion.bumpFiles,
     {
-      filename: "data/dnb/build/info.json",
-      type: "json",
+      filename: 'data/dnb/build/info.json',
+      type: 'json',
     },
   ],
 };

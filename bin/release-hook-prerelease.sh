7103971dd6e068194406a9ba28d4c4ea400fe571
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2021-12-31 13:37:22 +0700

    chore: fix release script

--- bin/release-hook-prerelease.sh
+++ bin/release-hook-prerelease.sh
@@ -29,7 +29,7 @@ hugo mod tidy
 git add go.mod
 git add go.sum
 
-if test -f "$SCRIPTPATH"/"replacem"e"nts; the"n
+if test -f "$SCRIPTPATH"/replacements; then
   while read -ra __; do
     go mod edit -replace "${__[0]}"="${__[1]}"
   done < "$SCRIPTPATH"/replacements

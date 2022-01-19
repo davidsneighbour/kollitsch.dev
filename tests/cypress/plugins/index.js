31f3bcbccfde9213a81046cbec5a34fece87755b
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2022-01-19 20:04:21 +0700

    feat: install, setup and configure cypress testing

---
+++ tests/cypress/plugins/index.js
@@ -0,0 +1,5 @@
+// noinspection JSUnresolvedVariable
+module.exports = (on, config) =>
+  // eslint-disable-next-line global-require
+  // eslint-disable-next-line unicorn/prefer-module
+  require('@dnb-org/cypress-config')(config.configFile);

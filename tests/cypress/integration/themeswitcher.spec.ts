efaf14c09b562e998ef62028efa0730f7fcca61f
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2022-02-03 19:04:02 +0700

    tests: configure testing for theme switcher

---
+++ tests/cypress/integration/themeswitcher.spec.ts
@@ -0,0 +1,39 @@
+describe('Testing Theme Switcher', () => {
+  it('check default theme setup', () => {
+
+    cy.visit('/').then(() => {
+
+      const expected = (
+        window.matchMedia &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches
+      ) ? 'dim' : 'light';
+
+      expect(localStorage.getItem('theme')).to.eq(expected);
+
+    });
+
+    cy.clearLocalStorage('theme').then((ls) => {
+      expect(ls.getItem('theme')).to.be.null
+    })
+
+    // test available themeswitcher links
+    cy.get('.is--themeswitcher a').each((element: HTMLElement, index, $list) => {
+      cy.get(element[0]).click().should(() => {
+        expect(localStorage.getItem('theme')).to.eq(element[0].id)
+      })
+    });
+  })
+
+  // test themes by id
+  it('Check all themes by name', () => {
+    const themes = ['light', 'dim', 'dark'];
+    cy.visit('/')
+    themes.forEach(theme => {
+      cy.get('#' + theme).click().should((element) => {
+        expect(localStorage.getItem('theme')).to.eq(theme)
+        //expect(element).get('body').should("have.attr", "class", theme)
+      })
+    })
+  });
+
+})

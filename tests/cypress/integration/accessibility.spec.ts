45ef02f5e342923c0eec57b54fd7fcde379b6e24
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-02-03 19:04:02 +0700

    tests: configure testing for theme switcher

---
+++ tests/cypress/integration/accessibility.spec.ts
@@ -0,0 +1,41 @@
+const links = require('../../../public/links.json');
+const items = links.links;
+
+const terminalLog = (violations) => {
+  cy.task(
+    'log',
+    `${violations.length} accessibility violation${violations.length === 1 ? '' : 's'
+    } ${violations.length === 1 ? 'was' : 'were'} detected`
+  )
+  const violationData = violations.map(
+    ({ id, impact, description, nodes }) => ({
+      id,
+      impact,
+      description,
+      nodes: nodes.length
+    })
+  )
+  cy.task('table', violationData)
+}
+
+describe('Component accessibility test', () => {
+  items.forEach((route) => {
+    const testName = `${route} has no detectable accessibility violations on load`;
+    it(testName, () => {
+      cy.visit(route);
+      cy.injectAxe();
+      cy.get('body').each((element, index) => {
+        cy.checkA11y(
+          'body',
+          {
+            runOnly: {
+              type: 'tag',
+              values: ['wcag21aa'],
+            },
+          },
+          terminalLog,
+        );
+      });
+    });
+  });
+});

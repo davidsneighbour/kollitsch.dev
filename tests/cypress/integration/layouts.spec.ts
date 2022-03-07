7d238b9872ad78da579e5077752ee77a91e8d740
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2022-03-07 17:48:27 +0700

    tests(fix): check homepage for title in h1 instead of /blog/

--- tests/cypress/integration/layouts.spec.ts
+++ tests/cypress/integration/layouts.spec.ts
@@ -1,6 +1,6 @@
 describe('Checking layouts', () => {
   it('shows the section layout on layout page', () => {
-    cy.visit('/blog/');
+    cy.visit('/');
     cy.get('h1').should('contain', 'kollitsch.de');
   });
 });

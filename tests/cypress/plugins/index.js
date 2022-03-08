70b34153dd343a6a82d1e307bf44fbfd78ed8d6c
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-03-08 20:41:58 +0700

    tests: non-working html validator in cypress

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -1,5 +1,38 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires, import/no-extraneous-dependencies
+const htmlvalidate = require('cypress-html-validate/dist/plugin');
+
+module.exports = (on) => {
+  on('task', {
+    seedRisks() {
+      console.log('sdjdsj'); // just a placeholder
+    },
+  });
+};
+
+
+
+module.exports = (
+  /** @type {(action: string, arg: Record<string, Function>) => void} */ on
+) => {
+  /* html-validate configuration */
+  const config = {
+    rules: {},
+  };
+  /* plugin options */
+  const options = {
+    exclude: [],
+    include: [],
+    formatter(messages) {
+      console.log(messages);
+    },
+  };
+  htmlvalidate.install(on, config, options);
+};
+
 // noinspection JSUnresolvedVariable
-module.exports = (on, config) =>
-  // eslint-disable-next-line global-require
-  // eslint-disable-next-line unicorn/prefer-module
+module.exports = (
+  /** @type {any} */ on,
+  /** @type {{ configFile: any; }} */ config
+) =>
+  // eslint-disable-next-line @typescript-eslint/no-var-requires, global-require, @typescript-eslint/no-var-requires
   require('@dnb-org/cypress-config')(config.configFile);
--- tests/cypress/support/index.js
+++ tests/cypress/support/index.js
@@ -1,17 +1,4 @@
-// ***********************************************************
-// This example support/index.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
+/* eslint-disable import/no-extraneous-dependencies */
 import 'cypress-axe';
-import "./commands";
+import 'cypress-html-validate/dist/commands';
+import './commands';

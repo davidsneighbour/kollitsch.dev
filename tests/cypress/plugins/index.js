e1fae59bead54e783b2839358ee961dc4bb0bc84
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2022-03-09 17:11:33 +0700

    tests: fix html validator integration

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -1,38 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires, import/no-extraneous-dependencies
+/* eslint-disable global-require, import/no-extraneous-dependencies, @typescript-eslint/no-var-requires */
 const htmlvalidate = require('cypress-html-validate/dist/plugin');
+const loadConfig = require('@dnb-org/cypress-config');
 
-module.exports = (on) => {
-  on('task', {
-    seedRisks() {
-      console.log('sdjdsj'); // just a placeholder
-    },
-  });
+module.exports = (on, config) => {
+  htmlvalidate.install(on);
+  return loadConfig(config.configFile);
 };
-
-
-
-module.exports = (
-  /** @type {(action: string, arg: Record<string, Function>) => void} */ on
-) => {
-  /* html-validate configuration */
-  const config = {
-    rules: {},
-  };
-  /* plugin options */
-  const options = {
-    exclude: [],
-    include: [],
-    formatter(messages) {
-      console.log(messages);
-    },
-  };
-  htmlvalidate.install(on, config, options);
-};
-
-// noinspection JSUnresolvedVariable
-module.exports = (
-  /** @type {any} */ on,
-  /** @type {{ configFile: any; }} */ config
-) =>
-  // eslint-disable-next-line @typescript-eslint/no-var-requires, global-require, @typescript-eslint/no-var-requires
-  require('@dnb-org/cypress-config')(config.configFile);

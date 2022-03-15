693b1146a75f699d239fad40d9996e9d3f75b93a
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-03-15 19:48:40 +0700

    tests: updating testing setup

--- tests/cypress/integration/themeswitcher.spec.ts
+++ tests/cypress/integration/themeswitcher.spec.ts
@@ -1,39 +1,45 @@
 describe('Testing Theme Switcher', () => {
   it('check default theme setup', () => {
-
     cy.visit('/').then(() => {
-
-      const expected = (
+      const expected =
         window.matchMedia &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches
-      ) ? 'dark' : 'light';
+          window.matchMedia('(prefers-color-scheme: dark)').matches
+          ? 'dark'
+          : 'light';
 
       expect(localStorage.getItem('theme')).to.eq(expected);
-
     });
 
     cy.clearLocalStorage('theme').then((ls) => {
-      expect(ls.getItem('theme')).to.be.null
-    })
+      expect(ls.getItem('theme')).to.be.null;
+    });
 
     // test available themeswitcher links
-    cy.get('.is--themeswitcher a').each((element: HTMLElement, index, $list) => {
-      cy.get(element[0]).click().should(() => {
-        expect(localStorage.getItem('theme')).to.eq(element[0].id)
-      })
-    });
-  })
-
-  // test themes by id
-  it('Check all themes by name', () => {
-    const themes = ['light', 'dark'];
-    cy.visit('/')
-    themes.forEach(theme => {
-      cy.get('#' + theme).click().should((element) => {
-        expect(localStorage.getItem('theme')).to.eq(theme)
-        //expect(element).get('body').should("have.attr", "class", theme)
-      })
-    })
+    cy.get('.is--themeswitcher a').each(
+      (element: HTMLElement, index, $list) => {
+        cy.get(element[0])
+          .click()
+          .should(() => {
+            expect(localStorage.getItem('theme')).to.eq(element[0].id);
+          });
+      }
+    );
   });
 
-})
+  // test themes by id
+  it('check all themes by name', () => {
+    const themes = ['light', 'dark'];
+    cy.visit('/');
+    themes.forEach((theme) => {
+      cy.get(`#${theme}`)
+        .click()
+        .should((element) => {
+          expect(localStorage.getItem('theme')).to.eq(theme);
+          // expect(element).get('body').should("have.attr", "class", theme)
+        });
+    });
+  });
+});
+
+// https://www.cypress.io/blog/2019/12/13/test-your-web-app-in-dark-mode/
+// https://relatablecode.com/how-to-make-a-cypress-test-for-prefers-color-scheme-dark/

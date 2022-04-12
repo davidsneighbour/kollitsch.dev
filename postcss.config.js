4a4b2b397f0d1fe8125f939a2e7f949f2f968783
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-04-13 00:19:35 +0700

    build(fix): remove purgecss for now

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,18 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./hugo_stats.json'],
-  defaultExtractor: (content) => {
-    const els = JSON.parse(content).htmlElements;
-    return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
-  },
-});
+// const purgecss = require('@fullhuman/postcss-purgecss')({
+//   content: ['./hugo_stats.json'],
+//   defaultExtractor: (content) => {
+//     const els = JSON.parse(content).htmlElements;
+//     return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
+//   },
+// });
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 const defaultConfig = require('@davidsneighbour/postcss-config');
 
 const localConfig = {
-  plugins: [
-    ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : []),
-  ],
+  // plugins: [
+  //   ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : []),
+  // ],
 };
 
 const config = {

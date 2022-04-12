85b37731d5761799e63ab60aa40a00638700e5ef
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2022-04-12 20:34:48 +0700

    build(frontmatter): update front matter configuration

--- assets/config/frontmatter/build.ts
+++ assets/config/frontmatter/build.ts
@@ -1,11 +1,13 @@
 const fs = require('node:fs');
+
 const path = require('node:path');
 
 const defaults = fs.readFileSync(path.join(__dirname, '/defaults.json'));
 const defaultConfiguration = JSON.parse(defaults.toString());
-
 const files = fs.readdirSync(path.join(__dirname, '/types/'));
+const files2 = fs.readdirSync(path.join(__dirname, '/partials/'));
 
+let partialsConfiguration = {};
 let typeConfiguration = {};
 
 files.forEach(function mergeConfigs(value) {
@@ -15,10 +17,6 @@ files.forEach(function mergeConfigs(value) {
     ...JSON.parse(data),
   };
 });
-
-const files2 = fs.readdirSync(path.join(__dirname, '/partials/'));
-
-let partialsConfiguration = {};
 
 files2.forEach(function mergeConfigs(value) {
   const data = fs.readFileSync(path.join(__dirname, `/partials/${value}`));

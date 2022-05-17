f0281a20d17c1a7a670bcf98bbeca9c0d63a5ab2
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2022-05-18 00:25:05 +0700

    tests: update test cases, testing for hugo template errors

---
+++ tests/cypress/integration/hugo-template-audit.ts
@@ -0,0 +1,40 @@
+const links = require('../../../public/links.json');
+
+const items = links.links;
+
+// see discourse.gohugo.io/t/audit-your-published-site-for-problems/35184
+//
+// <!-- raw HTML omitted -->
+// By default, Hugo strips raw HTML from your markdown prior to rendering,
+// and leaves this HTML comment in its place.
+//
+// ZgotmplZ
+// ZgotmplZ is a special value that indicates that unsafe content reached
+// a CSS or URL context at runtime.
+// For more information see https://pkg.go.dev/html/template.
+//
+// HAHAHUGO
+// Under conditions too complex to explain in this article, a rendered
+// shortcode may include all or a portion of the string HAHAHUGOSHORTCODE
+// in either uppercase or lowercase. This is difficult to detect in all
+// cicumstances because, depending on a variety of factors, the rendered
+// shortcode may include H, HA, HAH, HAHA, HAHAH, HAHAHU, HAHAHUG,
+// HAHAHUGO, etc. A case-insensitive search of the output for HAHAHUGO is
+// likely to catch the majority of cases without producing false positives.
+//
+// [i18n]
+// This is the placeholder produced instead of the default value or an
+// empty string if a translation is missing.
+
+describe('Testing layouts', () => {
+	items.forEach((route): string => {
+		const testName = `${route} has no detectable html violations on load`;
+		it(testName, () => {
+			cy.visit(route);
+			cy.get('html').should('not.contain', 'ZgotmplZ');
+			cy.get('html').should('not.contain', '<!-- raw HTML omitted -->');
+			cy.get('html').should('not.contain', 'HAHAHUGO');
+			cy.get('html').should('not.contain', '[i18n]');
+		});
+	});
+});
--- tests/cypress/integration/themeswitcher.spec.ts
+++ tests/cypress/integration/themeswitcher.spec.ts
@@ -1,44 +1,39 @@
 describe('Testing Theme Switcher', () => {
-  it('check default theme setup', () => {
-    cy.visit('/').then(() => {
-      const expected =
-        window.matchMedia &&
-          window.matchMedia('(prefers-color-scheme: dark)').matches
-          ? 'dark'
-          : 'light';
+	it('check default theme setup', () => {
+		cy.visit('/').then(() => {
+			const expected =
+				window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 
-      expect(localStorage.getItem('theme')).to.eq(expected);
-    });
+			expect(localStorage.getItem('theme')).to.eq(expected);
+		});
 
-    cy.clearLocalStorage('theme').then((ls) => {
-      expect(ls.getItem('theme')).to.be.null;
-    });
+		cy.clearLocalStorage('theme').then((ls) => {
+			expect(ls.getItem('theme')).to.be.null;
+		});
 
-    // test available themeswitcher links
-    cy.get('.is--themeswitcher a').each(
-      (element: HTMLElement, index, $list) => {
-        cy.get(element[0])
-          .click()
-          .should(() => {
-            expect(localStorage.getItem('theme')).to.eq(element[0].id);
-          });
-      }
-    );
-  });
+		// test available themeswitcher links
+		cy.get('.is--themeswitcher span.is--clickable').each((element: HTMLElement, index, $list) => {
+			cy.get(element[0])
+				.click()
+				.should(() => {
+					expect(localStorage.getItem('theme')).to.eq(element[0].id);
+				});
+		});
+	});
 
-  // test themes by id
-  it('check all themes by name', () => {
-    const themes = ['light', 'dark'];
-    cy.visit('/');
-    themes.forEach((theme) => {
-      cy.get(`#${theme}`)
-        .click()
-        .should((element) => {
-          expect(localStorage.getItem('theme')).to.eq(theme);
-          // expect(element).get('body').should("have.attr", "class", theme)
-        });
-    });
-  });
+	// test themes by id
+	it('check all themes by name', () => {
+		const themes = ['light', 'dark'];
+		cy.visit('/');
+		themes.forEach((theme) => {
+			cy.get(`#${theme}`)
+				.click()
+				.should((element) => {
+					expect(localStorage.getItem('theme')).to.eq(theme);
+					// expect(element).get('body').should("have.attr", "class", theme)
+				});
+		});
+	});
 });
 
 // https://www.cypress.io/blog/2019/12/13/test-your-web-app-in-dark-mode/

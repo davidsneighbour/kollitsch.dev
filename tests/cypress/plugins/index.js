90f6006614114bddbd6ec276c47186cc80d09daa
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-05-22 18:22:20 +0700

    tests(config): use IP and PORT for test server from .env variables

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -1,8 +1,14 @@
-/* eslint-disable global-require, import/no-extraneous-dependencies, @typescript-eslint/no-var-requires */
+// eslint-disable-next-line import/no-extraneous-dependencies
 const htmlvalidate = require('cypress-html-validate/dist/plugin');
 const loadConfig = require('@davidsneighbour/cypress-config');
+require('dotenv').config();
+
+const { PORT } = process.env;
+const { IP } = process.env;
 
 module.exports = (on, config) => {
   htmlvalidate.install(on);
-  return loadConfig(config.configFile);
+  const configuration = loadConfig(config.configFile);
+  configuration.baseUrl = `http://${IP}:${PORT}`;
+  return configuration;
 };

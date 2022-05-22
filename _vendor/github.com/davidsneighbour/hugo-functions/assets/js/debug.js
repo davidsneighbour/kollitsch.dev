87cd68038951b1ac1bb577518ac091060e006df8
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-05-22 14:13:34 +0700

    chore: remove _vendor directory

--- _vendor/github.com/davidsneighbour/hugo-functions/assets/js/debug.js
+++
@@ -1,6 +0,0 @@
-const debug = function() {
-  if (!window.console || !console.log) {
-    return;
-  }
-  return Function.prototype.bind.call(console.log, console);
-}();
--- _vendor/github.com/davidsneighbour/hugo-icons/content/dnb/bootstrap-icons.md
+++
@@ -1,5 +0,0 @@
----
-title: David's Neighbour Hugo Libs Bootstrap Icons Overview
-layout: bootstrap-icons-overview
-draft: true
----

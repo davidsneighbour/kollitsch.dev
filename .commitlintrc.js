2995bd6ed6519561f6c0119c3014629c82a8eb7d
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-05-30 20:38:32 +0700

    chore(config): update cz-git configuration

---
+++ .commitlintrc.js
@@ -0,0 +1,70 @@
+/** @type {import('cz-git').UserConfig} */
+module.exports = {
+  rules: {
+    // @see: https://commitlint.js.org/#/reference-rules
+  },
+  // https://cz-git.qbenben.com/guide/options-show.html
+  prompt: {
+    messages: {
+      type: "Select the type of change that you're committing:",
+      scope: "Denote the SCOPE of this change (optional):",
+      customScope: "Denote the SCOPE of this change:",
+      subject: "Write a SHORT, IMPERATIVE tense description of the change:\n",
+      body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+      breaking: 'List any BREAKING CHANGES (optional). Use "|" to break new line:\n',
+      footerPrefixsSelect: "Select the ISSUES type of changeList by this change (optional):",
+      customFooterPrefixs: "Input ISSUES prefix:",
+      footer: "List any ISSUES by this change. E.g.: #31, #34:\n",
+      confirmCommit: "Are you sure you want to proceed with the commit above?"
+    },
+    // https://github.com/sindresorhus/xterm-colors
+    themeColorCode: "38;5;160",
+    types: [
+      {
+         value: "feat",
+         name: "feat:     A new feature",
+         emoji: ":sparkles:"
+      },
+      { value: "fix",      name: "fix:      A bug fix", emoji: ":bug:" },
+      { value: "docs",     name: "docs:     Documentation only changes", emoji: ":memo:" },
+      { value: "style", name: "style:    Changes that do not affect the meaning of the code", emoji: ":lipstick:" },
+      { value: "refactor", name: "refactor: A code change that neither fixes a bug nor adds a feature", emoji: ":recycle:" },
+      { value: "perf", name: "perf:     A code change that improves performance", emoji: ":zap:" },
+      { value: "test", name: "test:     Adding missing tests or correcting existing tests", emoji: ":white_check_mark:" },
+      { value: "build", name: "build:    Changes that affect the build system or external dependencies", emoji: ":package:" },
+      { value: "ci", name: "ci:       Changes to our CI configuration files and scripts", emoji: ":ferris_wheel:" },
+      { value: "chore", name: "chore:    Other changes that don't modify src or test files", emoji: ":hammer:" },
+      { value: "revert", name: "revert:   Reverts a previous commit", emoji: ":rewind:" }
+    ],
+    useEmoji: false,
+    customScopesAlign: "bottom-top",
+    scopes: [],
+    allowCustomScopes: true,
+    allowEmptyScopes: true,
+    customScopesAlias: "custom",
+    emptyScopesAlias: "empty",
+    upperCaseSubject: false,
+    allowBreakingChanges: ['feat', 'fix'],
+    breaklineNumber: 100,
+    breaklineChar: "|",
+    skipQuestions: [],
+    issuePrefixs: [
+      { value: "closed", name: "closed:   ISSUE has been resolved" },
+      { value: "see",    name: "see:      ISSUE is connected to this commit" }
+    ],
+    customIssuePrefixsAlign: "top",
+    emptyIssuePrefixsAlias: "skip",
+    customIssuePrefixsAlias: "custom",
+    allowCustomIssuePrefixs: true,
+    allowEmptyIssuePrefixs: true,
+    confirmColorize: true,
+    maxHeaderLength: Infinity,
+    maxSubjectLength: Infinity,
+    minSubjectLength: 0,
+    scopeOverrides: undefined,
+    defaultBody: "",
+    defaultIssues: "",
+    defaultScope: "",
+    defaultSubject: ""
+  }
+}

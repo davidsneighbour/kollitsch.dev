446804fd271ec72782709148f688ac7c74f7cfa0
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2022-06-01 22:39:42 +0700

    chore(config): update postcss configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,48 @@
-// const purgecss = require('@fullhuman/postcss-purgecss')({
-//   content: ['./hugo_stats.json'],
-//   defaultExtractor: (content) => {
-//     const els = JSON.parse(content).htmlElements;
-//     return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
-//   },
-// });
+const cssnano = require('cssnano');
+const postcsspresetenv = require('postcss-preset-env');
+const autoprefixer = require('autoprefixer');
+const doiuse = require('doiuse');
+const stylelint = require('stylelint');
 
-// eslint-disable-next-line import/no-extraneous-dependencies
-const defaultConfig = require('@davidsneighbour/postcss-config');
-
-const localConfig = {
-  // plugins: [
-  //   ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : []),
-  // ],
+module.exports = {
+  plugins: [
+    // https://github.com/stylelint/stylelint/blob/main/docs/user-guide/usage/postcss-plugin.md
+    stylelint(
+      {
+        extends: '@davidsneighbour/stylelint-config',
+        ignoreFiles: [
+          'assets/scss/settings/_bootstrap-variables.scss',
+        ],
+      },
+    ),
+    // https://github.com/anandthakker/doiuse
+    doiuse({
+      browsers: 'extends @davidsneighbour/browserslist-config',
+      ignore: ['rem'],
+      ignoreFiles: ['**/normalize.css'],
+    }),
+    // https://github.com/postcss/autoprefixer
+    autoprefixer(),
+    // https://github.com/csstools/postcss-plugins/tree/main/plugin-packs/postcss-preset-env
+    postcsspresetenv({
+      stage: 2,
+      browsers: 'extends @davidsneighbour/browserslist-config',
+      // https://github.com/csstools/postcss-plugins/blob/main/plugin-packs/postcss-preset-env/FEATURES.md
+      features: {
+        'nesting-rules': true,
+      },
+      debug: true,
+    }),
+    // https://github.com/cssnano/cssnano
+    cssnano({
+      preset: [
+        'default',
+        {
+          discardComments: {
+            removeAll: true,
+          },
+        },
+      ],
+    }),
+  ],
 };
-
-const config = {
-  ...defaultConfig,
-  ...localConfig,
-};
-module.exports = config;

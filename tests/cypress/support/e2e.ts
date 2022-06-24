8fb71e73de209fcfd5a8b163a04217ec3c49f2e6
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2022-06-24 21:33:47 +0700

    tests: cypress 10 setup

--- tests/cypress/support/e2e.ts
+++ tests/cypress/support/e2e.ts
@@ -1,50 +1,50 @@
 import 'cypress-axe';
-import 'cypress-html-validate/commands';
+import 'cypress-html-validate/dist/commands';
 import './commands.ts';
 
 // @ts-ignore
 before(() => {
-  // root-level hook
-  // runs once before all tests
+	// root-level hook
+	// runs once before all tests
 });
 
 // @ts-ignore
 beforeEach(() => {
-  // root-level hook
-  // runs before every test block
+	// root-level hook
+	// runs before every test block
 });
 
 // @ts-ignore
 afterEach(() => {
-  // runs after each test block
-  // @ts-ignore
-  // cy.htmlvalidate();
+	// runs after each test block
+	// @ts-ignore
+	// cy.htmlvalidate();
 });
 
 // @ts-ignore
 after(() => {
-  // runs once all tests are done
+	// runs once all tests are done
 });
 
 // @ts-ignore
 describe('Hooks', () => {
-  // @ts-ignore
-  before(() => {
-    // runs once before all tests in the block
-  });
+	// @ts-ignore
+	before(() => {
+		// runs once before all tests in the block
+	});
 
-  // @ts-ignore
-  beforeEach(() => {
-    // runs before each test in the block
-  });
+	// @ts-ignore
+	beforeEach(() => {
+		// runs before each test in the block
+	});
 
-  // @ts-ignore
-  afterEach(() => {
-    // runs after each test in the block
-  });
+	// @ts-ignore
+	afterEach(() => {
+		// runs after each test in the block
+	});
 
-  // @ts-ignore
-  after(() => {
-    // runs once after all tests in the block
-  });
+	// @ts-ignore
+	after(() => {
+		// runs once after all tests in the block
+	});
 });

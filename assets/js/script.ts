1f29772bd01b2de38130cacc713c23b0798517f5
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2022-07-27 20:51:47 +0700

    fix: getting rid of light mode (for now)

--- assets/js/script.ts
+++ assets/js/script.ts
@@ -6,52 +6,52 @@ import { Collapse, Tab } from 'bootstrap';
 import 'web-vitals-element';
 
 // change Giscus theme
-function changeGiscusTheme(theme = 'dark_dimmed') {
-  let scheme = 'dark_dimmed';
-	if (theme === 'light') {
-		scheme = 'light';
-	}
+// function changeGiscusTheme(theme = 'dark_dimmed') {
+//   let scheme = 'dark_dimmed';
+// 	if (theme === 'light') {
+// 		scheme = 'light';
+// 	}
 
-	function sendMessage(message: { setConfig: { theme: string } }) {
-		const iframe = document.querySelector('iframe.giscus-frame') as HTMLIFrameElement;
-		if (iframe) {
-			// @ts-ignore
-			iframe.contentWindow.postMessage({ giscus: message }, 'https://giscus.app');
-		}
-	}
+// 	function sendMessage(message: { setConfig: { theme: string } }) {
+// 		const iframe = document.querySelector('iframe.giscus-frame') as HTMLIFrameElement;
+// 		if (iframe) {
+// 			// @ts-ignore
+// 			iframe.contentWindow.postMessage({ giscus: message }, 'https://giscus.app');
+// 		}
+// 	}
 
-	sendMessage({
-		setConfig: {
-			theme: scheme,
-		},
-	});
-}
+// 	sendMessage({
+// 		setConfig: {
+// 			theme: scheme,
+// 		},
+// 	});
+// }
 
 // @ts-ignore
 window.Alpine = Alpine;
 
 // themechanger
-Alpine.store('theme', {
-	mode: 'dark',
+// Alpine.store('theme', {
+// 	mode: 'dark',
 
-	init() {
-		const mode = localStorage.theme;
-		if (mode !== undefined) {
-			this.set(localStorage.theme);
-		} else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
-			this.set('light');
-		} else {
-			this.set('dark');
-		}
-	},
+// 	init() {
+// 		const mode = localStorage.theme;
+// 		if (mode !== undefined) {
+// 			this.set(localStorage.theme);
+// 		} else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+// 			this.set('light');
+// 		} else {
+// 			this.set('dark');
+// 		}
+// 	},
 
-	set(mode: string) {
-		this.mode = mode;
-		localStorage.theme = this.mode;
-		changeGiscusTheme(mode);
-		setTimeout(changeGiscusTheme, 2000, mode);
-	},
-});
+// 	set(mode: string) {
+// 		this.mode = mode;
+// 		localStorage.theme = this.mode;
+// 		changeGiscusTheme(mode);
+// 		setTimeout(changeGiscusTheme, 2000, mode);
+// 	},
+// });
 
 Alpine.start();
 

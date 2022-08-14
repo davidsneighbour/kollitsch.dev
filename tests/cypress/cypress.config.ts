8912187362cf2d87572026e4dc963e0e2f538ad2
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2022-08-14 22:08:05 +0700

    chore(config): update various configs and dependencies

--- tests/cypress/cypress.config.ts
+++ tests/cypress/cypress.config.ts
@@ -2,21 +2,21 @@ import { defineConfig } from 'cypress';
 import htmlvalidate from 'cypress-html-validate/plugin';
 
 export default defineConfig({
-  extends: '@davidsneighbour/cypress-config/base-hugo.json',
+  extends: '@davidsneighbour/config/cypress-base-hugo.json',
   env: {},
   projectId: 'q89kap',
   e2e: {
-		baseUrl: `http://192.168.1.201:1313`,
-		specPattern: './tests/cypress/integration/**/*.spec.ts',
+    baseUrl: `http://192.168.1.201:1313`,
+    specPattern: './tests/cypress/integration/**/*.spec.ts',
     excludeSpecPattern: ['**/accessibility/*.spec.ts', '**/htmlvalidation/*.spec.js'],
     setupNodeEvents(on, config) {
       htmlvalidate.install(on);
-			return config;
+      return config;
     },
-		supportFile: './tests/cypress/support/e2e.ts',
+    supportFile: './tests/cypress/support/e2e.ts',
     downloadsFolder: './tests/cypress/downloads',
-		fixturesFolder: './tests/cypress/fixtures',
-		screenshotsFolder: './tests/cypress/screenshots',
-		videosFolder: './tests/cypress/videos',
+    fixturesFolder: './tests/cypress/fixtures',
+    screenshotsFolder: './tests/cypress/screenshots',
+    videosFolder: './tests/cypress/videos',
   },
 });

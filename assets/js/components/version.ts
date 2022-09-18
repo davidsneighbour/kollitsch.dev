5e93b2b9c5ab3af514c39fc3e45ccf44862cd91e
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-09-18 17:42:47 +0700

    feat(fix): properly using only js for dynamic data in footer version note

--- assets/js/components/version.ts
+++ assets/js/components/version.ts
@@ -1,10 +1,8 @@
 const data = require("../../data/build.json");
-const versionList = Array.prototype.slice.call(document.querySelectorAll('.is--version'));
+const versionList = Array.prototype.slice.call(document.querySelectorAll('.is--site-version a.version'));
 versionList.forEach((element: Element) => {
 	element.innerText = data.tag_name;
-	// const tabTrigger = new Tab(triggerElement);
-	// triggerElement.addEventListener('click', (event: { preventDefault: () => void }) => {
-	// 	event.preventDefault();
-	// 	tabTrigger.show();
-	// });
+	element.href = data.html_url;
+	element.parentNode.classList.add('d-inline-block');
+	element.parentNode.classList.remove('d-none');
 });

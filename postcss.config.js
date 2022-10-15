643a4a0c6d7ab5fcf0dc2e7e5721f31414724953
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-10-15 20:48:18 +0700

    feat(fix): proper purgecss setup

--- postcss.config.js
+++ postcss.config.js
@@ -2,62 +2,75 @@ const cssnano = require('cssnano');
 const postcsspresetenv = require('postcss-preset-env');
 const autoprefixer = require('autoprefixer');
 const doiuse = require('doiuse');
-const stylelint = require('stylelint');
+
+const purgecss = require('@fullhuman/postcss-purgecss');
 
 module.exports = {
-  plugins: [
-    // https://github.com/anandthakker/doiuse
-    doiuse({
-      browsers: [
-        ">= 0.5%",
-        "last 2 major versions",
-        "not dead",
-        "Chrome >= 60",
-        "Firefox >= 60",
-        "Firefox ESR",
-        "iOS >= 12",
-        "Safari >= 12",
-        "not op_mini all",
-        "not op_mob > 0",
-        "not opera > 0",
-      ],
-      ignore: ['rem'],
-      ignoreFiles: ['**/normalize.css'],
-    }),
-    // https://github.com/postcss/autoprefixer
-    autoprefixer(),
-    // https://github.com/csstools/postcss-plugins/tree/main/plugin-packs/postcss-preset-env
-    postcsspresetenv({
-      stage: 2,
-      browsers: [
-        ">= 0.5%",
-        "last 2 major versions",
-        "not dead",
-        "Chrome >= 60",
-        "Firefox >= 60",
-        "Firefox ESR",
-        "iOS >= 12",
-        "Safari >= 12",
-        "not op_mini all",
-        "not op_mob > 0",
-        "not opera > 0",
-      ],
-      // https://github.com/csstools/postcss-plugins/blob/main/plugin-packs/postcss-preset-env/FEATURES.md
-      features: {
-        'nesting-rules': true,
-      },
-      debug: true,
-    }),
-    // https://github.com/cssnano/cssnano
-    cssnano({
-      preset: [
-        'default',
-        {
-          discardComments: {
-            removeAll: true,
-          },
-        },
-      ],
-    }),
-  ],
+	plugins: [
+		// https://github.com/anandthakker/doiuse
+		doiuse({
+			browsers: [
+				">= 0.5%",
+				"last 2 major versions",
+				"not dead",
+				"Chrome >= 60",
+				"Firefox >= 60",
+				"Firefox ESR",
+				"iOS >= 12",
+				"Safari >= 12",
+				"not op_mini all",
+				"not op_mob > 0",
+				"not opera > 0",
+			],
+			ignore: ['rem'],
+			ignoreFiles: ['**/normalize.css'],
+		}),
+		// https://github.com/postcss/autoprefixer
+		autoprefixer(),
+		// https://github.com/csstools/postcss-plugins/tree/main/plugin-packs/postcss-preset-env
+		postcsspresetenv({
+			stage: 2,
+			browsers: [
+				">= 0.5%",
+				"last 2 major versions",
+				"not dead",
+				"Chrome >= 60",
+				"Firefox >= 60",
+				"Firefox ESR",
+				"iOS >= 12",
+				"Safari >= 12",
+				"not op_mini all",
+				"not op_mob > 0",
+				"not opera > 0",
+			],
+			// https://github.com/csstools/postcss-plugins/blob/main/plugin-packs/postcss-preset-env/FEATURES.md
+			features: {
+				'nesting-rules': true,
+			},
+			debug: true,
+		}),
+		purgecss(({
+			content: ['./hugo_stats.json'],
+			defaultExtractor: content => {
+				const els = JSON.parse(content).htmlElements;
+				return [
+					...(els.tags || []),
+					...(els.classes || []),
+					...(els.ids || []),
+				];
+			},
+			safelist: []
+		})),
+		// https://github.com/cssnano/cssnano
+		cssnano({
+			preset: [
+				'default',
+				{
+					discardComments: {
+						removeAll: true,
+					},
+				},
+			],
+		}),
+	],
 };

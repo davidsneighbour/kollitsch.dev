d63e2676c3cbdfa3559f32e46ccb2be7bf7c03b5
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-10-25 17:48:54 +0700

    chore(config): replace spaces with tabs

--- .versionrc.js
+++ .versionrc.js
@@ -1,44 +1,44 @@
 const defaultStandardVersion = {
-  // for available options see https://github.com/conventional-changelog/conventional-changelog-config-spec/blob/master/versions/2.1.0/README.md
-  scripts: {
-    prerelease: "./bin/release/prerelease",
-  },
-  bumpFiles: [
-    {
-      filename: "package.json",
-      type: "json",
-    },
-  ],
-  types: [
-    { type: "build", section: "Build System" },
-    { type: "chore", section: "Chore" },
-    { type: "ci", section: "CI" },
-    { type: "docs", section: "Documentation" },
-    { type: "feat", section: "Features" },
-    { type: "fix", section: "Bug Fixes" },
-    { type: "perf", section: "Performance" },
-    { type: "refactor", section: "Refactors" },
-    { type: "revert", section: "Reverts" },
-    { type: "style", section: "Styling" },
-    { type: "test", section: "Tests" },
-    { type: "theme", section: "Theme" },
-  ],
+	// for available options see https://github.com/conventional-changelog/conventional-changelog-config-spec/blob/master/versions/2.1.0/README.md
+	scripts: {
+		prerelease: "./bin/release/prerelease",
+	},
+	bumpFiles: [
+		{
+			filename: "package.json",
+			type: "json",
+		},
+	],
+	types: [
+		{ type: "build", section: "Build System" },
+		{ type: "chore", section: "Chore" },
+		{ type: "ci", section: "CI" },
+		{ type: "docs", section: "Documentation" },
+		{ type: "feat", section: "Features" },
+		{ type: "fix", section: "Bug Fixes" },
+		{ type: "perf", section: "Performance" },
+		{ type: "refactor", section: "Refactors" },
+		{ type: "revert", section: "Reverts" },
+		{ type: "style", section: "Styling" },
+		{ type: "test", section: "Tests" },
+		{ type: "theme", section: "Theme" },
+	],
 };
 const localStandardVersion = {
-  bumpFiles: [
-    ...defaultStandardVersion.bumpFiles,
-    {
-      filename: 'data/dnb/kollitsch/build.json',
-      type: 'json',
-    },
-  ],
-  skip: {
-    changelog: true
-  }
+	bumpFiles: [
+		...defaultStandardVersion.bumpFiles,
+		{
+			filename: 'data/dnb/kollitsch/build.json',
+			type: 'json',
+		},
+	],
+	skip: {
+		changelog: true
+	}
 };
 
 const standardVersion = {
-  ...defaultStandardVersion,
-  ...localStandardVersion,
+	...defaultStandardVersion,
+	...localStandardVersion,
 };
 module.exports = standardVersion;

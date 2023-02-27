6d9822eb0e21a73d826f8139a784e34e3e420cc4
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2023-02-27 20:45:24 +0700

    fix: changes to scripts in front matter

--- .frontmatter/scripts/retrieve-unsplash-header-image-info.js
+++ .frontmatter/scripts/retrieve-unsplash-header-image-info.js
@@ -5,30 +5,70 @@ require('dotenv').config();
 const arguments = process.argv;
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
+const Fs = require('fs');
+const Https = require('https');
+
+/**
+ * Download a file from the given `url` into the `targetFile`.
+ *
+ * @param {String} url
+ * @param {String} targetFile
+ *
+ * @returns {Promise<void>}
+ */
+async function downloadFile(url, targetFile) {
+	return await new Promise((resolve, reject) => {
+		Https.get(url, response => {
+			const code = response.statusCode ?? 0
+
+			if (code >= 400) {
+				return reject(new Error(response.statusMessage))
+			}
+
+			// handle redirects
+			if (code > 300 && code < 400 && !!response.headers.location) {
+				return resolve(
+					downloadFile(response.headers.location, targetFile)
+				)
+			}
+
+			// save the file to disk
+			const fileWriter = Fs
+				.createWriteStream(targetFile)
+				.on('finish', () => {
+					resolve({})
+				})
+
+			response.pipe(fileWriter)
+		}).on('error', error => {
+			reject(error)
+		})
+	})
+};
+
 (async () => {
-  if (arguments && arguments.length > 0) {
-    const folderArg = path.dirname(arguments[3]);
-    const frontmatterArg = arguments[4];
-    const data = frontmatterArg && typeof frontmatterArg === "string" ? JSON.parse(frontmatterArg) : null;
-    const imageId = data.unsplash.imageid;
+	if (arguments && arguments.length > 0) {
+		const folderArg = path.dirname(arguments[3]);
+		const frontmatterArg = arguments[4];
+		const data = frontmatterArg && typeof frontmatterArg === "string" ? JSON.parse(frontmatterArg) : null;
 
-    const dataFeed = util.format('https://api.unsplash.com/photos/%s?client_id=%s', imageId, process.env.UNSPLASH_ACCESS_KEY);
-    const writeFilePromise = util.promisify(fs.writeFile);
 
-    async function downloadFile(url, outputPath) {
-      const x = await fetch(url);
-      const x_1 = await x.arrayBuffer();
-      return await writeFilePromise(outputPath, Buffer.from(x_1));
-    }
+		const imageId = data.unsplash.imageid;
+		console.log(imageId);
 
-    const dataFile = './data/dnb/kollitsch/unsplash/' + imageId + '.json';
-    downloadFile(dataFeed, dataFile);
+		const dataFeed = util.format('https://api.unsplash.com/photos/%s?client_id=%s', imageId, process.env.UNSPLASH_ACCESS_KEY);
+		const writeFilePromise = util.promisify(fs.writeFile);
 
-    let rawdata = fs.readFileSync(dataFile);
-    let imageData = JSON.parse(rawdata);
-    downloadFile(imageData.urls.full, folderArg + "/header.jpg");
+		console.log(dataFeed);
 
-    console.log('{ "frontmatter": { "bla": "fasel" }}');
+		const dataFile = './data/dnb/unsplash/' + imageId + '.json';
+		await downloadFile(dataFeed, dataFile);
 
-  }
+		let rawdata = fs.readFileSync(dataFile);
+		let imageData = JSON.parse(rawdata);
+		await downloadFile(imageData.urls.full, folderArg + "/header.jpg");
+
+		console.log('{ "frontmatter": { "bla": "fasel" }}');
+
+	}
 })();

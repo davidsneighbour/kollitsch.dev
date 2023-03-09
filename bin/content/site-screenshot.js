a8040f63ac169327b438dce0619369540d325b49
Patrick Kollitsch <patrick@davids-neighbour.com>
2023-03-09 20:37:07 +0700

    fix: screenshot script is broken

--- bin/content/site-screenshot.js
+++ bin/content/site-screenshot.js
@@ -1,37 +1,13 @@
-import { chromium } from "playwright";
-import yargs from "yargs";
-
-const argv = yargs
-	.option("url", {
-		describe: "URL to capture a screenshot of",
-		demandOption: true,
-		type: "string"
-	})
-	.option("output", {
-		describe: "Output file name",
-		default: "header.jpg",
-		type: "string"
-	})
-	.option("width", {
-		describe: "Viewport width",
-		default: 1200,
-		type: "number"
-	})
-	.option("height", {
-		describe: "Viewport height",
-		default: 600,
-		type: "number"
-	})
-	.help()
-	.alias("help", "h").argv;
-
-export const takeScreenshot = async (url, output, width, height) => {
-	const browser = await chromium.launch();
-	const page = await browser.newPage();
-	await page.setViewportSize({ width, height });
-	await page.goto(url);
-	await page.screenshot({ path: output });
+const { chromium } = require("playwright");
+const argv = require('yargs').argv;
+(async () => {
+	let browser = await chromium.launch();
+	let page = await browser.newPage();
+	await page.setViewportSize({
+		width: argv.width || 1200,
+		height: argv.height || 600
+	});
+	await page.goto(argv.url || 'https://example.com');
+	await page.screenshot({ path: argv.output || 'header.jpg' });
 	await browser.close();
-};
-
-takeScreenshot(argv.url, argv.output, argv.width, argv.height);
+})();

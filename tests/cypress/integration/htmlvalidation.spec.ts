370aa950fd10db19a0dcbe1792e683e487c906f3
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2023-03-09 19:45:55 +0700

    chore: cleanup, moving things around

--- tests/cypress/integration/htmlvalidation.spec.ts
+++ tests/cypress/integration/htmlvalidation.spec.ts
@@ -3,8 +3,13 @@ const config = require('../../html-validator/config.json');
 
 const items = links.links;
 
+const randomizer = (inArray: any, num: number) => {
+  const shuffled = [...inArray].sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, num);
+}
+
 describe('Checking layouts', () => {
-  items.forEach((route) => {
+  items.forEach((route: string) => {
     const testName = `${route} has no detectable html violations on load`;
     it(testName, () => {
       cy.visit(route);

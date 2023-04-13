1d474637a8f1f01b4cd771f4827c9e11322ef349
Patrick Kollitsch <patrick@davids-neighbour.com>
2023-04-13 17:32:09 +0700

    chore(config): fix standard version setup

--- .versionrc.js
+++ .versionrc.js
@@ -24,21 +24,5 @@ const defaultStandardVersion = {
 		{ type: "theme", section: "Theme" },
 	],
 };
-const localStandardVersion = {
-	bumpFiles: [
-		...defaultStandardVersion.bumpFiles,
-		{
-			filename: 'data/dnb/kollitsch/build.json',
-			type: 'json',
-		},
-	],
-	skip: {
-		changelog: true
-	}
-};
 
-const standardVersion = {
-	...defaultStandardVersion,
-	...localStandardVersion,
-};
-module.exports = standardVersion;
+module.exports = defaultStandardVersion;

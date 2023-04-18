b6de02a8bfe85b2f6fb2d129b71d847eee4d340d
Patrick Kollitsch <patrick@davids-neighbour.com>
2023-04-18 19:27:52 +0700

    chore(deps): update dependencies

--- webpack.common.js
+++ webpack.common.js
@@ -1,69 +1,93 @@
 const path = require('path');
 
 class FileListPlugin {
-    apply(compiler) {
-        compiler.hooks.emit.tapAsync('FileListPlugin', (compilation, callback) => {
-            let a = { files: [] };
-            for (let filename in compilation.assets) {
-                a.files.push(filename);
-                let f = filename.split('.');
-                let filetype = f[f.length - 1];
-                if (filetype === 'css' || filetype === 'js')
-                    a[filetype] = {
-                        filename: filename,
-                        hash: f[f.length - 2]
-                    };
-            }
-            const a2 = JSON.stringify(a);
-            // Insert this list into the webpack build as a new file asset:
-            compilation.assets['filelist.json'] = {
-                source: () => { return a2 },
-                size: () => { return a2.length }
-            };
-            callback();
-        });
-    }
+	apply(compiler) {
+		compiler.hooks.emit.tapAsync('FileListPlugin', (compilation, callback) => {
+			let a = { files: [] };
+			for (let filename in compilation.assets) {
+				a.files.push(filename);
+				let f = filename.split('.');
+				let filetype = f[f.length - 1];
+				if (filetype === 'css' || filetype === 'js')
+					a[filetype] = {
+						filename: filename,
+						hash: f[f.length - 2]
+					};
+			}
+			const a2 = JSON.stringify(a);
+			// Insert this list into the webpack build as a new file asset:
+			compilation.assets['filelist.json'] = {
+				source: () => { return a2 },
+				size: () => { return a2.length }
+			};
+			callback();
+		});
+	}
 }
 
 module.exports = {
-    target: 'web',
-    entry: {
-        theme: './themes/wonderland/assets/css/theme.css',
-    },
-    output: {
-        path: path.resolve(__dirname, 'static/assets/theme'),
-        filename: '[name].[fullhash].js',
-        clean: true,
-    },
-    devtool: 'source-map',
-    performance: {
-        maxEntrypointSize: 100000,
-        maxAssetSize: 500000,
-        hints: 'warning'
-    },
-    plugins: [
-        new FileListPlugin(),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 1,
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader'
-                    }
-                ]
-            }
-        ]
-    }
+	target: 'web',
+	entry: {
+		theme: './themes/hugo-theme/assets/scss/style.scss',
+	},
+	output: {
+		path: path.resolve(__dirname, 'themes/hugo-theme/static/assets/theme'),
+		filename: '[name].[fullhash].js',
+		clean: true,
+	},
+	devtool: 'source-map',
+	performance: {
+		maxEntrypointSize: 100000,
+		maxAssetSize: 500000,
+		hints: 'warning'
+	},
+	plugins: [
+		new FileListPlugin(),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					{ loader: 'style-loader' },
+					{
+						loader: 'css-loader', options: {
+							sourceMap: true, modules: true,
+							localIdentName: '[local]_[hash:base64:5]'
+						}
+					},
+					{
+						loader: 'sass-loader', options: { sourceMap: true }
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							sourceMap: true,
+							config: {
+								path: 'postcss.config.js'
+							}
+						}
+					}
+				]
+			},
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'style-loader',
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 1,
+						}
+					},
+					{
+						loader: 'postcss-loader'
+					}
+				]
+			}
+		]
+	}
 };

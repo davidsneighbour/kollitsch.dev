ad4d8676c49de9d103e1b1059a52cc4168d72a98
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2023-04-20 21:14:30 +0700

    chore(config): fix hook script

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,1 +1,6 @@
-export default { "*.js": "eslint" }
+export default {
+	"*.js": "eslint",
+	"*.css": "stylelint",
+	"*.scss": "stylelint --syntax=scss",
+	"*.{png,jpeg,jpg,gif,svg}": "imagemin-lint-staged"
+}

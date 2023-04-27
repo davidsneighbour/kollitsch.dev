a98a429ec86b56d8f1f84085e28f2354dcfc8c8c
Patrick Kollitsch <patrick@davids-neighbour.com>
2023-04-27 22:22:01 +0700

    feat(fix): local stylesheets for giscus

--- themes/hugo-theme/assets/js/scripts/theme-toggle.ts
+++ themes/hugo-theme/assets/js/scripts/theme-toggle.ts
@@ -2,26 +2,24 @@
 // @todo create a react component for this
 const storageKey = 'theme-preference'
 
+// set giscus theme urls
+const url = window.location.href;
+const urlParsed = url.split("/");
+const giscusDarkTheme = urlParsed[0] + "//" + urlParsed[2] + '/hugo-giscus/dark.css';
+const giscusLightTheme = urlParsed[0] + "//" + urlParsed[2] + '/hugo-giscus/light.css';
+
 const onClick = () => {
-	// flip current value
-	theme.value = theme.value === 'light'
-		? 'dark'
-		: 'light'
-
-	setPreference()
-
-
+	theme.value = theme.value === 'light' ? 'dark' : 'light';
+	setPreference();
 	changeGiscusTheme();
-	setTimeout(changeGiscusTheme, 5000);
+	setTimeout(changeGiscusTheme, 1000);
 }
 
 const getColorPreference = () => {
-	if (localStorage.getItem(storageKey))
+	if (localStorage.getItem(storageKey)) {
 		return localStorage.getItem(storageKey);
-	else
-		return window.matchMedia('(prefers-color-scheme: dark)').matches
-			? 'dark'
-			: 'light';
+	}
+	return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 }
 
 const setPreference = () => {
@@ -31,54 +29,33 @@ const setPreference = () => {
 
 const reflectPreference = () => {
 	// @todo get embarrassed about the following 17 lines of code and refactor
-	document.firstElementChild.setAttribute('data-bs-theme', theme.value);
-	document.querySelector('body').classList.add(theme.value);
-	document.querySelector('body').classList.remove(theme.value === 'light' ? 'dark' : 'light');
+	document.firstElementChild?.setAttribute('data-bs-theme', theme.value);
+	document.querySelector('body')?.classList.add(theme.value);
+	document.querySelector('body')?.classList.remove(theme.value === 'light' ? 'dark' : 'light');
 
 	if (theme.value === 'dark') {
-		document.querySelector('#toggle-button-dark').classList.add('d-inline-block');
-		document.querySelector('#toggle-button-dark').classList.remove('d-none');
-		document.querySelector('#toggle-button-light').classList.add('d-none');
-		document.querySelector('#toggle-button-light').classList.remove('d-inline-block');
+		document.querySelector('#toggle-button-dark')?.classList.add('d-inline-block');
+		document.querySelector('#toggle-button-dark')?.classList.remove('d-none');
+		document.querySelector('#toggle-button-light')?.classList.add('d-none');
+		document.querySelector('#toggle-button-light')?.classList.remove('d-inline-block');
 	} else {
-		document.querySelector('#toggle-button-light').classList.add('d-inline-block');
-		document.querySelector('#toggle-button-light').classList.remove('d-none');
-		document.querySelector('#toggle-button-dark').classList.add('d-none');
-		document.querySelector('#toggle-button-dark').classList.remove('d-inline-block');
+		document.querySelector('#toggle-button-light')?.classList.add('d-inline-block');
+		document.querySelector('#toggle-button-light')?.classList.remove('d-none');
+		document.querySelector('#toggle-button-dark')?.classList.add('d-none');
+		document.querySelector('#toggle-button-dark')?.classList.remove('d-inline-block');
 	}
 	document.querySelector('#theme-toggle')?.setAttribute('aria-label', theme.value);
 }
 
-const theme = {
-	value: getColorPreference(),
-}
-
-// set early so no page flashes / CSS is made aware
-reflectPreference()
-
-window.onload = () => {
-	// set on load so screen readers can see latest value on the button
-	reflectPreference()
-
-	// now this script can find and listen for clicks on the control
-	document.querySelector('#theme-toggle').addEventListener('click', onClick)
-}
-
-// sync with system changes
-window
-	.matchMedia('(prefers-color-scheme: dark)')
-	.addEventListener('change', ({ matches: isDark }) => {
-		theme.value = isDark ? 'dark' : 'light'
-		setPreference()
-	})
-
 // @todo move into hugo-giscus component
-function changeGiscusTheme() {
-	const theme = document.firstElementChild.getAttribute('data-bs-theme') === 'dark' ? 'dark' : 'light'
+const changeGiscusTheme = () => {
+	const theme = document.firstElementChild?.getAttribute('data-bs-theme') === 'dark' ? giscusDarkTheme : giscusLightTheme;
 
-	function sendMessage(message) {
+	function sendMessage(message: object) {
 		const iframe = document.querySelector('iframe.giscus-frame');
-		if (!iframe) return;
+		if (!iframe) {
+			return;
+		}
 		iframe.contentWindow.postMessage({ giscus: message }, 'https://giscus.app');
 	}
 
@@ -89,6 +66,66 @@ function changeGiscusTheme() {
 	});
 
 }
+
+const theme = {
+	value: getColorPreference(),
+}
+reflectPreference()
+window.onload = () => {
+	reflectPreference();
+	document.querySelector('#theme-toggle').addEventListener('click', onClick);
+}
+
+window
+	.matchMedia('(prefers-color-scheme: dark)')
+	.addEventListener('change', ({ matches: isDark }) => {
+		theme.value = isDark ? 'dark' : 'light'
+		setPreference()
+	});
+
 changeGiscusTheme();
 setTimeout(changeGiscusTheme, 5000);
 
+
+// const modeToggle = document.getElementsByClassName("mode-toggle")[0];
+// if (typeof modeToggle !== "undefined") {
+//   modeToggle.addEventListener('click', changeGiscusTheme);
+// }
+
+
+// (function() {
+// 	var dmmq = window.matchMedia('(prefers-color-scheme: dark)');
+// 	var giscusFrame;
+// 	function updateGiscus() {
+// 			if (!giscusFrame) giscusFrame = document.querySelector('iframe.giscus-frame');
+// 			if (dmmq.matches) {
+// 					// Dark theme
+// 					giscusFrame.contentWindow.postMessage({ giscus: { setConfig: { theme: 'dark' } } }, 'https://giscus.app');
+// 			} else {
+// 					// Light theme
+// 					giscusFrame.contentWindow.postMessage({ giscus: { setConfig: { theme: 'light' } } }, 'https://giscus.app');
+// 			}
+// 	}
+// 	setTimeout(updateGiscus, 1000); // leave time for giscus to load
+// 	dmmq.addEventListener('change', updateGiscus);
+// })();
+
+
+// function changeGiscusTheme() {
+// 	// theme can either be `dark` or `light`.
+// 	let theme = localStorage.getItem("pref-theme");
+// 	let message = { setConfig: { theme: theme } };
+// 	let iframe = document.querySelector('iframe.giscus-frame');
+// 	// iframe might not be loaded by the time this function is called.
+// 	if (iframe) {
+// 			iframe.contentWindow.postMessage({ giscus: message }, 'https://giscus.app');
+// 	}
+// }
+
+// function handleMessage(event) {
+// 	if (event.origin !== 'https://giscus.app') return;
+// 	if (!(typeof event.data === 'object' && event.data.giscus)) return;
+// 	changeGiscusTheme();
+// }
+
+// window.addEventListener('message', handleMessage);

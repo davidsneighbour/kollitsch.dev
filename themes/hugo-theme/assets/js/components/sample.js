3ca554c5681b18c312cca93a0cac896eeeb09ff7
Patrick Kollitsch <patrick@davids-neighbour.com>
2023-05-01 21:14:44 +0700

    chore: remove hugo-theme module

--- themes/hugo-theme/assets/js/components/sample.js
+++
@@ -1,8 +0,0 @@
-export default function Profile() {
-	return (
-		<img
-			src="https://i.imgur.com/MK3eW3Am.jpg"
-			alt="Katherine Johnson"
-		/>
-	)
-}
--- themes/hugo-theme/assets/js/scripts/logger.ts
+++
@@ -1,5 +0,0 @@
-const logger = {
-	...console,
-	success: (...args) => console.log("\x1b[32msuccess\x1b[0m", ...args),
-	failure: (...args) => console.error("\x1b[31mfailure\x1b[0m", ...args),
-};

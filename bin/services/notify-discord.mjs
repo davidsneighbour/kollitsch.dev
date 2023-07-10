743514c6d3d41023ad7385aba0ba9c93efb7be49
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2023-07-10 19:02:54 +0700

    chore(cleanup): move things around

--- bin/services/notify-discord.mjs
+++ bin/services/notify-discord.mjs
@@ -1,3 +1,5 @@
+// https://www.npmjs.com/package/node-fetch
+// https://stackabuse.com/making-http-requests-in-node-js-with-node-fetch/
 import fetch from 'node-fetch';
 import 'dotenv/config';
 
@@ -30,3 +32,50 @@ fetch(FEED_LINK)
 			sendMessage(message);
 		});
 	});
+
+
+// https://developer.mozilla.org/en-US/docs/Web/API/fetch#options
+
+// (async () => {
+// 	const response = await fetch('https://webscrapingapi.com');
+// 	const body = await response.text();
+// 	console.log(body);
+// })();
+
+// (async () => {
+// 	const response = await fetch('http://httpbin.org/post', {
+// 			method: 'POST',
+// 			body: JSON.stringify({
+// 					'key': 'value'
+// 			})
+// 	});
+// 	const body = await response.text();
+// 	console.log(body);
+// })();
+
+
+// (async () => {
+// 	try {
+// 			const response = await fetch('[INVALID_URL]');
+// 			const responseBody = await response.text();
+// 	} catch (error) {
+// 			console.log(error.message);
+// 	}
+// })();
+
+// fetch('[INVALID_URL]')
+// 	.then((response) => response.text())
+// 	.then((body) => {
+// 			console.log(body);
+// 	})
+// 	.catch((error) => {
+// 			console.log(error.message);
+// 	});
+
+// const cheerio = require("cheerio");
+// (async () => {
+//     const response = await fetch('https://www.webscrapingapi.com/');
+//     const responseBody = await response.text();
+//     const $ = cheerio.load(responseBody);
+//     console.log($('title').first().text());
+// })();

5ed4cbc55fefbb1021e78878b00a449a0b73e593
Patrick Kollitsch <patrick@davids-neighbour.com>
2023-07-10 19:02:54 +0700

    chore(cleanup): move things around

--- bin/services/notify-mastodon.mjs
+++ bin/services/notify-mastodon.mjs
@@ -1,4 +1,5 @@
-// todo document
+// @todo document
+// @see https://docs.joinmastodon.org/methods/statuses/#create
 import fetch from 'node-fetch';
 import 'dotenv/config';
 
@@ -12,6 +13,8 @@ function sendMessage(message) {
 		headers: {
 			'Content-Type': 'application/json',
 			'Authorization': 'Bearer ' + MASTODON_ACCESS_TOKEN
+			// 			Idempotency-Key
+			// Provide this header with any arbitrary string to prevent duplicate submissions of the same status. Consider using a hash or UUID generated client-side. Idempotency keys are stored for up to 1 hour.
 		},
 		body: JSON.stringify({
 			status: message
@@ -23,8 +26,22 @@ fetch(FEED_LINK)
 	.then((response) => response.text())
 	.then((data) => {
 		parseString(data, (error, result) => {
+			console.log(error);
+			console.log(result);
+
 			let message = `New post on ${result.rss.channel[0].title[0]}`;
 			message += ` titled: [${result.rss.channel[0].item[0].title[0]}](${result.rss.channel[0].item[0].link[0]})`;
 			sendMessage(message);
 		});
 	});
+
+// @todo response
+
+// 	200: OK
+// 401: Unauthorized
+// Invalid or missing Authorization header.
+
+// {
+//   "error": "The access token is invalid"
+// }
+// 422: Unprocessable entity

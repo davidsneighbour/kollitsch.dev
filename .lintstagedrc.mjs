b78eecab7fe043de569245d20ba2dc7f985bb3f9
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2023-08-28 22:17:39 +0700

    ci(fix): allow https host github.com

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -3,27 +3,27 @@
 import { ESLint } from 'eslint'
 
 const removeIgnoredFiles = async (files) => {
-	const eslint = new ESLint()
-	const isIgnored = await Promise.all(
-		files.map((file) => {
-			return eslint.isPathIgnored(file)
-		})
-	)
-	const filteredFiles = files.filter((_, i) => !isIgnored[i])
-	return filteredFiles.join(' ')
+  const eslint = new ESLint()
+  const isIgnored = await Promise.all(
+    files.map((file) => {
+      return eslint.isPathIgnored(file)
+    })
+  )
+  const filteredFiles = files.filter((_, i) => !isIgnored[i])
+  return filteredFiles.join(' ')
 }
 
 export default {
-	'package-lock.json': "lockfile-lint --path package-lock.json --validate-https",
-	'*.{ts,tsx,(m|c)js,jsx}': async (files) => {
-		const filesToLint = await removeIgnoredFiles(files)
-		return [`eslint --max-warnings=0 ${filesToLint}`]
-	},
-	// "*.{md,markdown}": "markdownlint-cli2",
-	"*.{scss,css}": "stylelint --fix",
-	"*.{png,jpeg,jpg,gif,svg}": "imagemin-lint-staged",
-	"*.{js,jsx}": "flow focus-check",
-	"!(CHANGELOG)**/*.{md,markdown}": ["npm run lint:vale"],
-	'**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
-	//'**/*.js?(x)': (filenames) => filenames.map((filename) => `prettier --write '${filename}'`),
+  'package-lock.json': "lockfile-lint --path package-lock.json --validate-https --allowed-hosts github.com",
+  '*.{ts,tsx,(m|c)js,jsx}': async (files) => {
+    const filesToLint = await removeIgnoredFiles(files)
+    return [`eslint --max-warnings=0 ${filesToLint}`]
+  },
+  // "*.{md,markdown}": "markdownlint-cli2",
+  "*.{scss,css}": "stylelint --fix",
+  "*.{png,jpeg,jpg,gif,svg}": "imagemin-lint-staged",
+  "*.{js,jsx}": "flow focus-check",
+  "!(CHANGELOG)**/*.{md,markdown}": ["npm run lint:vale"],
+  '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
+  //'**/*.js?(x)': (filenames) => filenames.map((filename) => `prettier --write '${filename}'`),
 }

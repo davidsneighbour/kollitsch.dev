e48dae53ab556429a4e47814de396f6fd529a49a
Patrick Kollitsch <patrick@davids-neighbour.com>
2023-09-17 17:55:38 +0700

    chore(config): update configurations

--- .remarkrc.mjs
+++ .remarkrc.mjs
@@ -2,23 +2,23 @@ import remarkFrontmatter from 'remark-frontmatter';
 import remarkLintFrontmatterSchema from 'remark-lint-frontmatter-schema';
 
 const remarkConfig = {
-	plugins: [
-		[
-			"@davidsneighbour/remark-config",
-			["remark-lint-write-good", false]
-		],
-		remarkFrontmatter,
-		[
-			remarkLintFrontmatterSchema,
-			{
-				schemas: {
-					'./static/_schemata/**.schema.yaml': [
-						'./content/**/*.{md,mdx}',
-					],
-				},
-			},
-		],
-	],
+  plugins: [
+    [
+      "@davidsneighbour/remark-config",
+      ["remark-lint-write-good", false]
+    ],
+    remarkFrontmatter,
+    [
+      remarkLintFrontmatterSchema,
+      {
+        schemas: {
+          './static/_schemata/**.schema.yaml': [
+            './content/**/*.{md,mdx}',
+          ],
+        },
+      },
+    ],
+  ],
 };
 
 export default remarkConfig;

fd659c7dcecc328905c7fde71f08fa5a305a3009
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2023-10-30 19:47:26 +0700

    wip: service binaries

--- bin/services/notify.discord.mjs
+++ bin/services/notify.discord.mjs
@@ -11,14 +11,9 @@ const { FEED_LINK, DISCORD_WEBHOOK } = process.env;
 
 const main = async () => {
   try {
-    // Load feed using the loadFeed function
     const feed = await loadFeed(FEED_LINK);
-
-    // Create a message for Discord
     let message = `New post on ${feed.rss.channel[0].title[0]}`;
     message += ` titled: [${feed.rss.channel[0].item[0].title[0]}](${feed.rss.channel[0].item[0].link[0]})`;
-
-    // Send the message to Discord using the sendMessage function
     sendMessage(DISCORD_WEBHOOK, message);
   } catch (error) {
     console.error('Error:', error);

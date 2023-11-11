a059c3016305fd6aa30203ad38b9d35adcb3ba41
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2023-11-11 21:41:53 +0700

    build(ci): fix screenshot script

--- bin/services/site-screenshot.mjs
+++ bin/services/site-screenshot.mjs
@@ -5,42 +5,49 @@ import version from "../../package.json" assert { type: "json" };
 
 const argv = yargs(hideBin(process.argv))
 
-	.option("url", {
-		describe: "URL to capture a screenshot of",
-		demandOption: true,
-		type: "string"
-	})
-	.option("output", {
-		describe: "Output file name",
-		default: "header.jpg",
-		type: "string"
-	})
-	.option("width", {
-		describe: "Viewport width",
-		default: 1200,
-		type: "number"
-	})
-	.option("height", {
-		describe: "Viewport height",
-		default: 600,
-		type: "number"
-	})
-	.help()
-	.version(version.version)
-	.alias("help", "h").argv;
+  .option("url", {
+    describe: "URL to capture a screenshot of",
+    demandOption: true,
+    type: "string"
+  })
+  .option("output", {
+    describe: "Output file name",
+    default: "header.jpg",
+    type: "string"
+  })
+  .option("width", {
+    describe: "Viewport width",
+    default: 1200,
+    type: "number"
+  })
+  .option("height", {
+    describe: "Viewport height",
+    default: 600,
+    type: "number"
+  })
+  .help()
+  .version(version.version)
+  .alias("help", "h").argv;
 
 export const takeScreenshot = async (url, output, width, height) => {
 
-	const browser = await puppeteer.launch({ headless: "new", defaultViewport: { width: 1200, height: 800 } });
-	const page = await browser.newPage();
+  const browser = await puppeteer.launch({ headless: "new", defaultViewport: { width: 1200, height: 800 } });
+  const page = await browser.newPage();
 
-	await page.goto(url);
-	await page.emulateMediaFeatures([{
-		name: 'prefers-color-scheme', value: 'dark'
-	}]);
-	await page.screenshot({ path: output });
-	await browser.close();
+  try {
+    await page.goto(url, { waitUntil: 'load', timeout: 0 });
+  } catch (e) {
+    console.log('Error: ' + url + ' not available');
+    console.log(e.message);
+  }
+
+  await page.emulateMediaFeatures([{
+    name: 'prefers-color-scheme', value: 'dark'
+  }]);
+  await page.screenshot({ path: output });
+  await browser.close();
 };
 
 // @ts-ignore
 takeScreenshot(argv.url, argv.output, argv.width, argv.height);
+

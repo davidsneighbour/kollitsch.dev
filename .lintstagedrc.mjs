14d035ec6f13f5d5d4a67d5fc52fdf5fa230307a
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-01-22 19:12:05 +0700

    chore(config): update lintstaged configuration

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -13,14 +13,25 @@ const removeIgnoredFiles = async (/** @type {any[]} */ files) => {
 }
 
 export default {
-  'package-lock.json': 'lockfile-lint --path package-lock.json --validate-https --allowed-hosts npm',
+  'package-lock.json': [
+    'lockfile-lint --path package-lock.json --validate-https --allowed-hosts npm',
+  ],
   '*.{ts,tsx,(m|c)js,jsx}': async (/** @type {any} */ files) => {
     const filesToLint = await removeIgnoredFiles(files)
     return [`eslint --max-warnings=0 ${filesToLint}`]
   },
   '*.{scss,css}': ['stylelint --fix'],
-  '*.{png,jpeg,jpg,gif,svg}': ['imagemin-lint-staged'],
-  '!(CHANGELOG)**/*.{md,markdown}': ['markdownlint-cli2', 'npm run lint:vale'],
-  '**/*.ts?(x)': () => ['tsc -p tsconfig.json --noEmit'],
-  '**/*.*': ['npx secretlint']
+  '*.{png,jpeg,jpg,gif,svg}': [
+    'imagemin-lint-staged' // @davidsneighbour/imagemin-lint-staged
+  ],
+  '!(CHANGELOG)**/*.{md,markdown}': [
+    'markdownlint-cli2',
+    'npm run lint:vale'
+  ],
+  '**/*.ts?(x)': () => [
+    'tsc -p tsconfig.json --noEmit'
+  ],
+  '**/*.*': [
+    'npx secretlint'
+  ]
 }

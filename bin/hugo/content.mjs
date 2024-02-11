605a837892747000662fb6f78f0f1148010ed34e
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-02-11 19:30:42 +0700

    fix: cleanup content script and archetypes

--- bin/hugo/content.mjs
+++ bin/hugo/content.mjs
@@ -1,12 +1,12 @@
 // @see https://github.com/natemoo-re/clack/tree/main/packages/prompt
 import {
-	intro,
-	outro,
-	select,
-	spinner,
-	isCancel,
-	cancel,
-	text,
+  intro,
+  outro,
+  select,
+  spinner,
+  isCancel,
+  cancel,
+  text,
 } from '@clack/prompts';
 
 // @todo trying to find out how this really is supposed to work
@@ -14,191 +14,190 @@ import { exec as exec } from 'node:child_process';
 import fs from 'node:fs';
 
 async function prepareSlug(title) {
-	const slug_pre = title.replace(/\s+/g, '-').toLowerCase();
-	const slug = slug_pre.replace(/[^a-zA-Z0-9\-]/g, '');
-	return slug;
+  const slug_pre = title.replace(/\s+/g, '-').toLowerCase();
+  const slug = slug_pre.replace(/[^a-zA-Z0-9\-]/g, '');
+  return slug;
 }
 
 async function main() {
 
-	const date = new Date();
-	const year = date.getFullYear();
-	const spin = spinner();
-	let command;
-	let command2;
-	let title;
-	let slug, slug_pre;
-	let post;
+  const date = new Date();
+  const year = date.getFullYear();
+  const spin = spinner();
+  let command;
+  let command2;
+  let title;
+  let slug, slug_pre;
+  let post;
 
-	intro('Create content:');
+  intro('Create content:');
 
-	const contentType = await select({
-		message: 'Pick a content type.',
-		options: [
-			{ value: "post", label: "Post" },
-			{ value: "video", label: "Video Post" },
-			{ value: "component", label: "Component", hint: "Documentation page for a Hugo component" },
-			{ value: "m2p2", label: "Music to program to" },
-			{ value: "labnotes", label: "Notes from the Lab" },
-			{ value: "tag", label: "Tag", hint: "Tag description page" },
-			{ value: "quit", label: "Quit" },
-		]
-	});
+  const contentType = await select({
+    message: 'Pick a content type.',
+    options: [
+      { value: "post", label: "Post" },
+      { value: "video", label: "Video Post" },
+      { value: "m2p2", label: "Music to program to" },
+      { value: "labnotes", label: "Notes from the Lab" },
+      { value: "tag", label: "Tag", hint: "Tag description page" },
+      { value: "quit", label: "Quit" },
+    ]
+  });
 
-	if (isCancel(contentType)) {
-		cancel('Operation cancelled');
-		return process.exit(0);
-	};
+  if (isCancel(contentType)) {
+    cancel('Operation cancelled');
+    return process.exit(0);
+  };
 
-	switch (contentType) {
+  switch (contentType) {
 
-		case 'quit':
-			cancel('Operation cancelled');
-			return process.exit(0);
+    case 'quit':
+      cancel('Operation cancelled');
+      return process.exit(0);
 
-		case 'component':
-			title = await text({
-				message: 'Component Name:',
-				validate(value) {
-					if (value.length === 0) return `Title is required, doh!`;
-				}
-			});
-			slug = await prepareSlug(title);
-			post = `components/${slug}`;
-			command = `hugo new --kind components ${post}`;
-			command2 = `code content/${post}/index.md`;
-			break;
+    case 'component':
+      title = await text({
+        message: 'Component Name:',
+        validate(value) {
+          if (value.length === 0) return `Title is required, doh!`;
+        }
+      });
+      slug = await prepareSlug(title);
+      post = `components/${slug}`;
+      command = `hugo new --kind components ${post}`;
+      command2 = `code content/${post}/index.md`;
+      break;
 
-		case 'm2p2':
-			let number;
-			let increment;
-			const filename = "data/dnb/kollitsch/m2p2.json";
-			await /** @type {Promise<void>} */(new Promise((resolve, _reject) => {
-				fs.readFile(filename, 'utf8', function (err, buf) {
-					if (err) {
-						console.log(err);
-						cancel(`An error occured: ${err}`);
-						process.exit(0);
-					}
-					let data = JSON.parse(buf);
-					number = data.latest;
-					console.log(number);
-					increment = number + 1;
-					console.log(increment);
-					data.latest = increment;
-					fs.writeFile(filename, JSON.stringify(data, null, 2), (err) => {
-						if (err) console.log(err);
-					});
-					resolve();
-				});
-			}));
-			post = `blog/${year}/music-to-program-to-` + increment;
-			command = `hugo new --kind music2program2 ${post}`;
-			command2 = `code content/${post}/index.md`;
-			break;
+    case 'm2p2':
+      let number;
+      let increment;
+      const filename = "data/dnb/kollitsch/m2p2.json";
+      await /** @type {Promise<void>} */(new Promise((resolve, _reject) => {
+        fs.readFile(filename, 'utf8', function (err, buf) {
+          if (err) {
+            console.log(err);
+            cancel(`An error occured: ${err}`);
+            process.exit(0);
+          }
+          let data = JSON.parse(buf);
+          number = data.latest;
+          console.log(number);
+          increment = number + 1;
+          console.log(increment);
+          data.latest = increment;
+          fs.writeFile(filename, JSON.stringify(data, null, 2), (err) => {
+            if (err) console.log(err);
+          });
+          resolve();
+        });
+      }));
+      post = `blog/${year}/music-to-program-to-` + increment;
+      command = `hugo new --kind music2program2 ${post}`;
+      command2 = `code content/${post}/index.md`;
+      break;
 
-		case 'labnotes':
-			title = await text({
-				message: 'Month:',
-				validate(value) {
-					if (value.length === 0) return `Month is required, doh!`;
-				}
-			});
-			slug = await prepareSlug(title);
-			post = `blog/${year}/notes-from-the-laboratory-${slug}`;
-			command = `hugo new --kind notes-from-the-laboratory ${post}`;
-			command2 = `code content/${post}/index.md`;
-			break;
+    case 'labnotes':
+      title = await text({
+        message: 'Month:',
+        validate(value) {
+          if (value.length === 0) return `Month is required, doh!`;
+        }
+      });
+      slug = await prepareSlug(title);
+      post = `blog/${year}/notes-from-the-laboratory-${slug}`;
+      command = `hugo new --kind notes-from-the-laboratory ${post}`;
+      command2 = `code content/${post}/index.md`;
+      break;
 
-		case 'tag':
-			title = await text({
-				message: 'Tag Title:',
-				validate(value) {
-					if (value.length === 0) return `Title is required, doh!`;
-				}
-			});
-			slug = await prepareSlug(title);
-			post = `tags/${slug}`;
-			command = `hugo new ${post}`;
-			command2 = `code content/${post}/_index.md`;
-			break;
+    case 'tag':
+      title = await text({
+        message: 'Tag Title:',
+        validate(value) {
+          if (value.length === 0) return `Title is required, doh!`;
+        }
+      });
+      slug = await prepareSlug(title);
+      post = `tags/${slug}`;
+      command = `hugo new ${post}`;
+      command2 = `code content/${post}/_index.md`;
+      break;
 
-		case 'video':
-			title = await text({
-				message: 'Post Slug (special characters will be removed):',
-				validate(value) {
-					if (value.length === 0) return `Slug is required, doh!`;
-				}
-			});
-			slug = await prepareSlug(title);
-			post = `blog/${year}/${slug}`;
-			command = `hugo new --kind video ${post}`;
-			command2 = `code content/${post}/index.md`;
-			break;
+    case 'video':
+      title = await text({
+        message: 'Post Slug (special characters will be removed):',
+        validate(value) {
+          if (value.length === 0) return `Slug is required, doh!`;
+        }
+      });
+      slug = await prepareSlug(title);
+      post = `blog/${year}/${slug}`;
+      command = `hugo new --kind video ${post}`;
+      command2 = `code content/${post}/index.md`;
+      break;
 
-		case 'post':
-		default:
-			title = await text({
-				message: 'Post Slug (special characters will be removed):',
-				validate(value) {
-					if (value.length === 0) return `Slug is required, doh!`;
-				}
-			});
-			slug = await prepareSlug(title);
-			post = `blog/${year}/${slug}`;
-			command = `hugo new --kind blog ${post}`;
-			command2 = `code content/${post}/index.md`;
-			break;
+    case 'post':
+    default:
+      title = await text({
+        message: 'Post Slug (special characters will be removed):',
+        validate(value) {
+          if (value.length === 0) return `Slug is required, doh!`;
+        }
+      });
+      slug = await prepareSlug(title);
+      post = `blog/${year}/${slug}`;
+      command = `hugo new --kind blog ${post}`;
+      command2 = `code content/${post}/index.md`;
+      break;
 
-	}
+  }
 
-	// @todo check if content file already exists and fail gracefully
-	spin.start('Creating content files...');
+  // @todo check if content file already exists and fail gracefully
+  spin.start('Creating content files...');
 
-	await /** @type {Promise<void>} */(new Promise((resolve, _reject) => {
-		exec(command, function (
-			error,
-			_stdout,
-			stderr
-		) {
-			if (error) {
-				console.log(error);
-				cancel(`An error occured: ${error}`);
-				process.exit(0);
-			}
-			if (stderr) {
-				console.log(stderr);
-				cancel(`An error occured: ${stderr}`);
-				process.exit(0);
-			}
-			resolve();
-		});
-	}));
+  await /** @type {Promise<void>} */(new Promise((resolve, _reject) => {
+    exec(command, function (
+      error,
+      _stdout,
+      stderr
+    ) {
+      if (error) {
+        console.log(error);
+        cancel(`An error occured: ${error}`);
+        process.exit(0);
+      }
+      if (stderr) {
+        console.log(stderr);
+        cancel(`An error occured: ${stderr}`);
+        process.exit(0);
+      }
+      resolve();
+    });
+  }));
 
-	spin.stop('Content created. Opening in VS Code...');
+  spin.stop('Content created. Opening in VS Code...');
 
-	await /** @type {Promise<void>} */(new Promise((resolve, _reject) => {
-		exec(command2, function (
-			error,
-			_stdout,
-			stderr
-		) {
-			if (error) {
-				console.log(error);
-				cancel(`An error occured: ${error}`);
-				process.exit(0);
-			}
-			if (stderr) {
-				console.log(stderr);
-				cancel(`An error occured: ${stderr}`);
-				process.exit(0);
-			}
-			resolve();
-		});
-	}));
+  await /** @type {Promise<void>} */(new Promise((resolve, _reject) => {
+    exec(command2, function (
+      error,
+      _stdout,
+      stderr
+    ) {
+      if (error) {
+        console.log(error);
+        cancel(`An error occured: ${error}`);
+        process.exit(0);
+      }
+      if (stderr) {
+        console.log(stderr);
+        cancel(`An error occured: ${stderr}`);
+        process.exit(0);
+      }
+      resolve();
+    });
+  }));
 
-	outro('All done :)');
-	process.exit(1);
+  outro('All done :)');
+  process.exit(1);
 
 }
 
---
+++ content/blog/2024/kurzschnitte-ii-4/index.md
@@ -0,0 +1,24 @@
+---
+title: Kurzschnitte II - Number 4
+description: ""
+draft: true
+date: 2024-02-10T22:51:07+07:00
+publishDate: 2024-02-10T22:51:07+07:00
+lastmod: 2024-02-11T14:14:47+07:00
+resources:
+- title: Photo by [Kelsy Gagnebin](https://unsplash.com/@kelsymichael) via [Unsplash](https://unsplash.com/)
+  src: header.jpg
+categories:
+- kurzschnitte
+tags:
+- kurzschnitte
+- bookmarks
+- 100DaysToOffload
+type: blog
+unsplash:
+  imageid: UcEzgZ6k19o
+---
+
+* Experiment with web animations using the interactive [Bouncy Ball](https://sparkbox.github.io/bouncy-ball/#vanilla-js) tutorial, which provides a hands-on approach to learning animation techniques with vanilla JavaScript, pure CSS and the Web Animations API, among many other methods. This resource is perfect for developers looking to understand the basics of web animations and apply them in their projects.
+
+{{< tagnavigation "kurzschnitte" >}}

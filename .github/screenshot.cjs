b9ccad2675a2c6af7b517cb870ae51d416645d9f
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2024-05-20 16:53:41 +0700

    build(workflow): add proper screenshot script

---
+++ .github/screenshot.cjs
@@ -0,0 +1,32 @@
+const playwright = require("playwright");
+const fs = require("fs");
+const path = require("path");
+
+async function getData(screenshotDir = "./.github/screenshots") {
+  const browser = await playwright.chromium.launch();
+  const context = await browser.newContext({
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+  });
+  const page = await context.newPage();
+
+  await page.emulateMedia({ colorScheme: 'dark' });
+  await page.setExtraHTTPHeaders({
+    'Accept-Language': 'en-US,en;q=0.9'
+  });
+  await page.goto("https://kollitsch.dev/");
+  await page.waitForTimeout(500);
+
+  // Ensure the directory exists
+  if (!fs.existsSync(screenshotDir)) {
+    fs.mkdirSync(screenshotDir, { recursive: true });
+  }
+
+  const timestamp = new Date().toISOString().replace(/[-:.]/g, "");
+  const screenshotPath = path.join(screenshotDir, `screenshot_${timestamp}.png`);
+
+  await page.screenshot({ path: screenshotPath, fullPage: true });
+  await browser.close();
+  console.log(`Screenshot saved to ${screenshotPath}`);
+}
+
+getData();

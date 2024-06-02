d2df42df0e579493bedb8322e6e7e697b37fbf29
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-06-02 19:55:53 +0700

    ci(wip): changes to screenshot script

--- .github/screenshot.cjs
+++ .github/screenshot.cjs
@@ -1,8 +1,8 @@
+const fs = require('fs');
 const playwright = require("playwright");
-const fs = require("fs");
 const path = require("path");
 
-async function getData(screenshotDir = "./.github/screenshots") {
+async function getData(screenshotDir = "./.github/screenshots", retries = 3) {
   const browser = await playwright.chromium.launch();
   const context = await browser.newContext({
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
@@ -13,30 +13,34 @@ async function getData(screenshotDir = "./.github/screenshots") {
   await page.setExtraHTTPHeaders({
     'Accept-Language': 'en-US,en;q=0.9'
   });
-  await page.goto("https://kollitsch.dev/");
-  await page.waitForTimeout(500);
 
-  // Ensure the directory exists
-  if (!fs.existsSync(screenshotDir)) {
-    fs.mkdirSync(screenshotDir, { recursive: true });
+  for (let attempt = 1; attempt <= retries; attempt++) {
+    try {
+      await page.goto("https://kollitsch.dev/", { waitUntil: 'networkidle' });
+      await page.waitForTimeout(500);
+
+      // Ensure the directory exists
+      if (!fs.existsSync(screenshotDir)) {
+        fs.mkdirSync(screenshotDir, { recursive: true });
+      }
+
+      const screenshotPathDefault = path.join(screenshotDir, `new-screenshot.png`);
+
+      // Take screenshot and save to both paths
+      await page.screenshot({ path: screenshotPathDefault, fullPage: true });
+      await browser.close();
+      console.log(`Screenshot saved to ${screenshotPathDefault}`);
+      return;
+    } catch (error) {
+      console.error(`Attempt ${attempt} failed: ${error.message}`);
+      if (attempt === retries) {
+        console.error('Max retries reached. Exiting.');
+        await browser.close();
+        process.exit(1);
+      }
+      console.log('Retrying...');
+    }
   }
-
-  const now = new Date();
-  const timestamp = now.getFullYear().toString() +
-    (now.getMonth() + 1).toString().padStart(2, '0') +
-    now.getDate().toString().padStart(2, '0') +
-    now.getHours().toString().padStart(2, '0') +
-    now.getMinutes().toString().padStart(2, '0') +
-    now.getSeconds().toString().padStart(2, '0');
-  const screenshotPathWithTimestamp = path.join(screenshotDir, `screenshot_${timestamp}.png`);
-  const screenshotPathDefault = path.join(screenshotDir, `screenshot.png`);
-
-  // Take screenshot and save to both paths
-  await page.screenshot({ path: screenshotPathWithTimestamp, fullPage: true });
-  await page.screenshot({ path: screenshotPathDefault, fullPage: true });
-  await browser.close();
-  console.log(`Screenshot saved to ${screenshotPathWithTimestamp} and ${screenshotPathDefault}`);
-
 }
 
 getData();

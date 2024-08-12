ee27f5e7bd04b5fa88c5b3409599173fee03147f
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2024-08-12 11:32:55 +0700

    build(fix): move frontmatter configuration into package

--- frontmatter.config.cjs
+++ frontmatter.config.cjs
@@ -1,16 +1,2 @@
-const path = require('path');
-require('dotenv').config({
-  path: path.join(__dirname, `.env`)
-});
-
-module.exports = async (config) => {
-  return {
-    ...config,
-    "frontMatter.extends": [
-      "https://dnbhub.xyz/frontmatter/settings.global.json"
-    ],
-    "frontMatter.preview.host": "https://" + process.env.HOSTNAME + ':' + process.env.PORT,
-    "frontMatter.taxonomy.seoTitleLength": process.env.SEO_TITLE_LENGTH,
-    "frontMatter.site.baseURL": "https://kollitsch.dev/",
-  }
-};
+import config from '@davidsneighbour/frontmatter-config';
+export default config;

1c2fbf75ff3edf412f1fd6284e68423a18f07ca4
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2024-09-06 11:41:32 +0700

    theme(fix): re-add purgecss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,28 +1,29 @@
-const cssnano = require('cssnano');
-const postcsspresetenv = require('postcss-preset-env');
-const autoprefixer = require('autoprefixer');
-const doiuse = require('doiuse');
+const cssnano = require("cssnano");
+const postcsspresetenv = require("postcss-preset-env");
+const autoprefixer = require("autoprefixer");
+// @ts-ignore
+// const doiuse = require("doiuse");
 
 // running purgecss only in production so we can use all available classes in development
-const purgecss = process.env.HUGO_ENVIRONMENT === 'production' ? require('@fullhuman/postcss-purgecss')({
-  content: ['./hugo_stats.json'],
-  // https://github.com/gohugoio/hugo/issues/10338
-  // https://discourse.gohugo.io/t/purgecss-and-highlighting/41021
-  safelist: {
-    greedy: [/highlight/, /chroma/, /widget--web-vitals/, /dark/]
-  },
-  fontFace: true,
-  //variables: true,
-  keyframes: true,
-  defaultExtractor: (/** @type {string} */ content) => {
-    const els = JSON.parse(content).htmlElements;
-    return [
-      ...(els.tags || []),
-      ...(els.classes || []),
-      ...(els.ids || []),
-    ];
-  }
-}) : null;
+const purgecss =
+  process.env.HUGO_ENVIRONMENT === "production"
+    ? // @ts-ignore
+      require("@fullhuman/postcss-purgecss")({
+        content: ["./hugo_stats.json"],
+        // https://github.com/gohugoio/hugo/issues/10338
+        // https://discourse.gohugo.io/t/purgecss-and-highlighting/41021
+        safelist: {
+          greedy: [/highlight/, /chroma/, /widget--web-vitals/, /dark/],
+        },
+        fontFace: true,
+        //variables: true,
+        keyframes: true,
+        defaultExtractor: (/** @type {string} */ content) => {
+          const els = JSON.parse(content).htmlElements;
+          return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
+        },
+      })
+    : null;
 
 module.exports = {
   plugins: [
@@ -34,25 +35,24 @@ module.exports = {
     //   ignore: ['rem'],
     //   ignoreFiles: ['**/normalize.css'],
     // }),
-    //purgecss,
+    purgecss,
     // https://github.com/postcss/autoprefixer
     autoprefixer(),
     // https://github.com/csstools/postcss-plugins/tree/main/plugin-packs/postcss-preset-env
+    // @ts-ignore
     postcsspresetenv({
       stage: 2,
-      browsers: [
-        "extends @davidsneighbour/browserslist-config",
-      ],
+      browsers: ["extends @davidsneighbour/browserslist-config"],
       // https://github.com/csstools/postcss-plugins/blob/main/plugin-packs/postcss-preset-env/FEATURES.md
       features: {
-        'nesting-rules': true,
+        "nesting-rules": true,
       },
       //debug: true,
     }),
     // https://github.com/cssnano/cssnano
     cssnano({
       preset: [
-        'default',
+        "default",
         {
           discardComments: {
             removeAll: true,

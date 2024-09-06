f1cde5776970d04daaaad423fa3796c70d1a7fb8
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2024-09-06 17:19:52 +0700

    refactor: move theme into module

--- themes/nakkhian/assets/js/scripts/bs-tooltips.ts
+++
@@ -1,8 +0,0 @@
-// import { createPopper } from '@popperjs/core';
-import { Tooltip } from 'bootstrap';
-
-// Bootstrap Tooltips
-var tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-var tooltipList = Array.from(tooltipTriggerList).map(function (tooltipTriggerEl) {
-  return new Tooltip(tooltipTriggerEl);
-});
--- themes/nakkhian/assets/js/scripts/keyboard-layout.ts
+++
@@ -1,6 +0,0 @@
-import { install } from '@github/hotkey'
-
-// setup all hotkeys on the page
-for (const el of document.querySelectorAll('[data-hotkey]')) {
-  install(el)
-}

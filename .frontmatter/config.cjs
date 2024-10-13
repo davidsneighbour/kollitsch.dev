976fd521215ed4088604f0a8038681d0a7221eaf
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2024-10-13 20:16:44 +0700

    chore(config): update frontmatter configuration

--- .frontmatter/config.cjs
+++ .frontmatter/config.cjs
@@ -1,2 +1,8 @@
-import config from "@davidsneighbour/frontmatter-config";
-export default config;
+const dnbConfig = require('@davidsneighbour/frontmatter-config');
+
+module.exports = async function (config) {
+  return {
+    ...dnbConfig,
+    ...config,
+  };
+};

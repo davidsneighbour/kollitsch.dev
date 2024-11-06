344b6fa177a8a49e6b3eea7b7c939154311ae38a
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2024-11-07 06:59:24 +0700

    tests(setup): update playwright testing setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,12 +22,15 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    //['json', {  outputFile: 'test-results.json' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-    baseURL: 'http://localhost:1313',
+    baseURL: 'https://localhost:1313',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',

1e7f0748c5416ae0824ba09b5ef8083f691397d1
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-11-07 07:02:04 +0700

    build(config): add admonitions to remark configuration

--- .remarkrc.mjs
+++ .remarkrc.mjs
@@ -1,20 +1,25 @@
-import remarkFrontmatter from 'remark-frontmatter';
-import remarkLintFrontmatterSchema from 'remark-lint-frontmatter-schema';
+import remarkFrontmatter from "remark-frontmatter";
+import remarkLintFrontmatterSchema from "remark-lint-frontmatter-schema";
 
 const remarkConfig = {
   plugins: [
     [
       "@davidsneighbour/remark-config",
-      ["remark-lint-write-good", false]
+      ["remark-lint-write-good", false],
+
+      [
+        "remark-lint-no-undefined-references",
+        {
+          allow: ["!NOTE", "!TIP", "!CAUTION", "!IMPORTANT", "!WARNING"],
+        },
+      ],
     ],
     remarkFrontmatter,
     [
       remarkLintFrontmatterSchema,
       {
         schemas: {
-          './static/_schemata/blog.schema.yaml': [
-            './content/blog/**/*.{md,mdx}',
-          ],
+          "./static/_schemata/blog.schema.yaml": ["./content/blog/**/*.{md,mdx}"],
         },
       },
     ],

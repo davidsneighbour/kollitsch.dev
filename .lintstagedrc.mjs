8b52729f51ce3e066f252d9bc36c6c7cc5f761b5
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2024-11-19 18:23:00 +0700

    build(fix): proper lintstaged config for biome

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,40 +1,23 @@
 // @see https://github.com/lint-staged/lint-staged
 
 export default {
-
   "package-lock.json": [
     "lockfile-lint --path package-lock.json --validate-https --allowed-hosts npm",
   ],
 
   "*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}": [
-    "biome check --files-ignore-unknown=true", // Check formatting and lint
-    "biome check --write --no-errors-on-unmatched", // Format, sort imports, lint, and apply safe fixes
-    "biome check --write --organize-imports-enabled=false --no-errors-on-unmatched", // format and apply safe fixes
-    "biome check --write --unsafe --no-errors-on-unmatched", // Format, sort imports, lints, apply safe/unsafe fixes
-    "biome format --write --no-errors-on-unmatched", // Format
-    "biome lint --write --no-errors-on-unmatched", // Lint and apply safe fixes
+    "biome check --write --no-errors-on-unmatched",
   ],
 
-  "*.{scss,css}": [
-    "stylelint --fix",
-    "prettier --write",
-  ],
+  "*.{scss,css}": ["stylelint --fix", "prettier --write"],
 
   // '*.{png,jpeg,jpg,gif,svg}': [
   //   'imagemin-lint-staged' // @davidsneighbour/imagemin-lint-staged
   // ],
 
-  "!(CHANGELOG)**/*.{md,markdown}": [
-    "markdownlint-cli2",
-    "npm run lint:vale",
-  ],
+  "!(CHANGELOG)**/*.{md,markdown}": ["markdownlint-cli2", "npm run lint:vale"],
 
-  "layouts/**/*.*": [
-    "./bin/hugo/refactor layouts",
-  ],
+  "layouts/**/*.*": ["./bin/hugo/refactor layouts"],
 
-  "*": [
-    "secretlint",
-  ],
-
+  "*": ["secretlint"],
 };

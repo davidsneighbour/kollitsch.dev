3ef2b0ffe9d5c8944af3f127a62647cc2d9e7ecd
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2024-11-19 18:12:32 +0700

    build(fix): remove eslint, add biome to lint-staged config

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,32 +1,40 @@
 // @see https://github.com/lint-staged/lint-staged
-import { ESLint } from "eslint";
-
-const removeIgnoredFiles = async (/** @type {any[]} */ files) => {
-  const eslint = new ESLint();
-  const isIgnored = await Promise.all(
-    files.map((/** @type {string} */ file) => {
-      return eslint.isPathIgnored(file);
-    }),
-  );
-  const filteredFiles = files.filter((/** @type {any} */ _, /** @type {string | number} */ i) => !isIgnored[i]);
-  return filteredFiles.join(" ");
-};
 
 export default {
-  // '*.{json,jsonc}': ['biome check --staged'],
-  "package-lock.json": ["lockfile-lint --path package-lock.json --validate-https --allowed-hosts npm"],
-  "*.{ts,tsx,(m|c)js,jsx}": async (/** @type {any} */ files) => {
-    const filesToLint = await removeIgnoredFiles(files);
-    return [
-      `eslint --max-warnings=0 ${filesToLint}`,
-      //'biome check --staged'
-    ];
-  },
-  "*.{scss,css}": ["stylelint --fix", "prettier --write"],
+
+  "package-lock.json": [
+    "lockfile-lint --path package-lock.json --validate-https --allowed-hosts npm",
+  ],
+
+  "*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}": [
+    "biome check --files-ignore-unknown=true", // Check formatting and lint
+    "biome check --write --no-errors-on-unmatched", // Format, sort imports, lint, and apply safe fixes
+    "biome check --write --organize-imports-enabled=false --no-errors-on-unmatched", // format and apply safe fixes
+    "biome check --write --unsafe --no-errors-on-unmatched", // Format, sort imports, lints, apply safe/unsafe fixes
+    "biome format --write --no-errors-on-unmatched", // Format
+    "biome lint --write --no-errors-on-unmatched", // Lint and apply safe fixes
+  ],
+
+  "*.{scss,css}": [
+    "stylelint --fix",
+    "prettier --write",
+  ],
+
   // '*.{png,jpeg,jpg,gif,svg}': [
   //   'imagemin-lint-staged' // @davidsneighbour/imagemin-lint-staged
   // ],
-  "!(CHANGELOG)**/*.{md,markdown}": ["markdownlint-cli2", "npm run lint:vale"],
-  "layouts/**/*.*": ["./bin/hugo/refactor layouts"],
-  "*": ["secretlint"],
+
+  "!(CHANGELOG)**/*.{md,markdown}": [
+    "markdownlint-cli2",
+    "npm run lint:vale",
+  ],
+
+  "layouts/**/*.*": [
+    "./bin/hugo/refactor layouts",
+  ],
+
+  "*": [
+    "secretlint",
+  ],
+
 };

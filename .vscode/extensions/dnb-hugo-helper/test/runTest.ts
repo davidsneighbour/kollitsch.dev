b1907c87b54ef91e1fd7ce43549f4e21f0c1c98a
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2025-04-18 07:32:51 +0700

    fix(vscode-extension): update compilation structure

--- .vscode/extensions/dnb-hugo-helper/test/runTest.ts
+++ .vscode/extensions/dnb-hugo-helper/test/runTest.ts
@@ -1,13 +1,16 @@
-import * as path from 'path';
 import * as os from 'os';
+import * as path from 'path';
 import { runTests } from 'vscode-test';
 
 async function main() {
   try {
     const extensionDevelopmentPath = path.resolve(__dirname, '../../');
-    const extensionTestsPath = path.resolve(__dirname, './suite/index');
+    const extensionTestsPath = path.resolve(__dirname, './suite/index.js');
     // Use a temp folder for user data so tests can run even if you have VS Code open
-    const userDataDir = path.join(os.tmpdir(), 'vscode-dnb-hugo-helper-test-user-data');
+    const userDataDir = path.join(
+      os.tmpdir(),
+      'vscode-dnb-hugo-helper-test-user-data',
+    );
 
     await runTests({
       extensionDevelopmentPath,
@@ -15,8 +18,8 @@ async function main() {
       launchArgs: [
         '--disable-extensions',
         `--user-data-dir=${userDataDir}`,
-        '--skip-release-version-check'
-      ]
+        '--skip-release-version-check',
+      ],
     });
   } catch (err) {
     console.error('❌ Error running tests', err);

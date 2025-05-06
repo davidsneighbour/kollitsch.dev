ecfe9726d6ec7e50b85407b55b23a7067d044379
Patrick Kollitsch <83281+davidsneighbour@users.noreply.github.com>
2025-05-06 15:54:11 +0700

    tests: enable firefox and webkit tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -55,14 +55,14 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
   ],
   webServer: {
     command: 'npm run server',

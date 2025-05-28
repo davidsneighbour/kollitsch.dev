01945dd501f598299f85d5ac77687ea500a151b0
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2025-05-29 06:15:31 +0700

    ci(fix): remove lockfile lint
    it provided hard to actually lint without blocking anything. plenty of packages have missing hashes
    or wrong formats and it makes no sense to block commits based on that. needs to be re-implemented
    somewhere else.

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,5 @@
 // @see https://github.com/lint-staged/lint-staged
 export default {
-	"package-lock.json": [
-		"lockfile-lint --path package-lock.json --validate-https --allowed-hosts npm",
-	],
-
 	"*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}": [
 		"biome check --write --no-errors-on-unmatched",
 	],

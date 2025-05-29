124af6071e0a1b2f443279d5465a52f458654dca
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2025-05-29 07:04:21 +0700

    theme(fix): sort and limit rss feed items

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,19 +1,26 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import { getCollection } from "astro:content";
+import rss from "@astrojs/rss";
+import sitedata from "@data/siteinfo.json";
 
 export async function GET(context) {
-  const blog = await getCollection('blog');
-  return rss({
-    title: 'Buzz’s Blog',
-    description: 'A humble Astronaut’s guide to the stars',
-    site: context.site,
-    items: blog.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      // Compute RSS link from post `id`
-      // This example assumes all posts are rendered as `/blog/[id]` routes
-      link: `/blog/${post.id}/`,
-    })),
-  });
+	let blog = await getCollection("blog", ({ data }) => {
+		return data?.draft !== true;
+	});
+	// ({ data }) => import.meta.env.DEV || !data?.draft)
+	blog.sort((a, b) => Date.parse(b.data.date) - Date.parse(a.data.date));
+	blog = blog.slice(0, 10);
+
+	return rss({
+		title: sitedata.title,
+		description: sitedata.description,
+		site: sitedata.url,
+		items: blog.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.pubDate,
+			description: post.data.description,
+			// Compute RSS link from post `id`
+			// This example assumes all posts are rendered as `/blog/[id]` routes
+			link: `/blog/${post.id}/`,
+		})),
+	});
 }

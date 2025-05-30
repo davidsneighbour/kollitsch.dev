d5a8df69cd9ceda080a6131262cb9e200d2f9f39
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2025-05-30 15:43:55 +0700

    theme(feat): add theme switcher component

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+// biome-ignore lint/correctness/noUnusedVariables: this is used for type definitions
+interface Window {
+  theme: {
+    setTheme: (theme: "auto" | "dark" | "light") => void;
+    getTheme: () => "auto" | "dark" | "light";
+    getSystemTheme: () => "light" | "dark";
+    getDefaultTheme: () => "auto" | "dark" | "light";
+  };
+}

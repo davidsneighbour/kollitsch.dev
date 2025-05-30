1d235a00f1141d6c95c98405a699605b5941a733
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2025-05-30 07:19:41 +0700

    ci: add prettier configuration for astro files

--- prettier.config.js
+++ prettier.config.js
@@ -7,31 +7,34 @@
  * @type {import("prettier").Config}
  */
 const config = {
-	printWidth: 100,
-	tabWidth: 2,
-	useTabs: false,
-	semi: true,
-	singleQuote: true,
-	quoteProps: "as-needed",
-	trailingComma: "es5",
-	bracketSpacing: true,
-	arrowParens: "avoid",
-	endOfLine: "lf",
-	plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
-	overrides: [
-		{
-			files: "*.html",
-			options: {
-				singleQuote: false,
-			},
-		},
-		{
-			files: "*.astro",
-			options: {
-				parser: "astro",
-			},
-		},
-	],
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  /**
+   * make sure that prettier-plugin-tailwindcss is at the end of the plugins array
+   */
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  overrides: [
+    {
+      files: '*.html',
+      options: {
+        singleQuote: false,
+      },
+    },
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
 };
 
 export default config;

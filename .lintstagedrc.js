3ce87c3f5ab8e6a9c144495f39d0f76f5120d84b
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2025-05-31 08:11:46 +0700

    build(fix): re-enable secretlint in lintstaged configuration

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,23 +1,25 @@
 // @see https://github.com/lint-staged/lint-staged
 export default {
-	"*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}": [
-		"biome check --write --no-errors-on-unmatched",
-	],
+  '*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}': [
+    'biome check --write --no-errors-on-unmatched',
+  ],
 
-	"*.{scss,css}": ["stylelint --fix", "prettier --write"],
+  '*.{scss,css}': ['stylelint --fix', 'prettier --write'],
 
-	// 'layouts/**/*.*': [
-	//   './bin/hugo/refactor layouts'
-	// ],
+  // 'layouts/**/*.*': [
+  //   './bin/hugo/refactor layouts'
+  // ],
 
-	// '*.jsonnet': ['jsonnetfmt --in-place'],
+  // '*.jsonnet': ['jsonnetfmt --in-place'],
 
-	"!(CHANGELOG)**/*.{md,markdown}": [
-		'markdownlint-cli2 --config "src/config/.markdownlint.json"',
-		'vale --config="src/config/vale/vale.ini" --no-exit',
-		"lychee --no-progress --config src/config/lychee.toml --require-https --format detailed --mode color -vv",
-		"typos --config src/config/typos.toml",
-	],
+  '!(CHANGELOG)**/*.{md,markdown}': [
+    'markdownlint-cli2 --config "src/config/.markdownlint.json"',
+    'vale --config="src/config/vale/vale.ini" --no-exit',
+    'lychee --no-progress --config src/config/lychee.toml --require-https --format detailed --mode color -vv',
+    'typos --config src/config/typos.toml',
+  ],
 
-	// "*": ["secretlint --secretlintrc src/config/.secretlintrc.json"],
+  '*': [
+    'secretlint --secretlintrc src/config/secretlint/.secretlintrc.json --secretlintignore src/config/secretlint/.secretlintignore',
+  ],
 };

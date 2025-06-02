9f169550b23bc8345e718894dc0e09374ded3038
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2025-06-02 10:00:51 +0700

    theme(feat): add .well-known/security.txt

---
+++ src/pages/.well-known/security.txt.js
@@ -0,0 +1,28 @@
+/**
+ * @todo update to latest security.txt spec
+ * @see https://securitytxt.org/
+ */
+export async function GET() {
+  // Compute Expires date one year from now (UTC)
+  const expires = new Date();
+  expires.setUTCFullYear(expires.getUTCFullYear() + 1);
+  const expiresIso = expires.toISOString();
+
+  // Build the body of security.txt
+  const body = `##############################################################################
+# Information related to reporting security vulnerabilities on kollitsch.dev #
+##############################################################################
+
+Expires: ${expiresIso}
+Contact: https://kollitsch.dev/contact/
+Preferred-Languages: en,de
+Policy: https://davids-neighbour.com/security-policy/
+`;
+
+  return new Response(body, {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8',
+    },
+  });
+}

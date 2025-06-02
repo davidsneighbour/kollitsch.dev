ac510f3a8c83799ae3d08b8fe517d329d110a3dc
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2025-06-02 09:49:15 +0700

    theme(fix): rss feed items and permalinks

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,26 +1,38 @@
-import { getCollection } from "astro:content";
-import rss from "@astrojs/rss";
-import sitedata from "@data/siteinfo.json";
+import { getCollection } from 'astro:content';
+import rss from '@astrojs/rss';
+import sitedata from '@data/siteinfo.json';
 
 export async function GET(context) {
-	let blog = await getCollection("blog", ({ data }) => {
-		return data?.draft !== true;
-	});
-	// ({ data }) => import.meta.env.DEV || !data?.draft)
-	blog.sort((a, b) => Date.parse(b.data.date) - Date.parse(a.data.date));
-	blog = blog.slice(0, 10);
+  let blog = await getCollection('blog', ({ data }) => {
+    return data?.draft !== true;
+  });
+  // ({ data }) => import.meta.env.DEV || !data?.draft)
+  blog.sort((a, b) => Date.parse(b.data.date) - Date.parse(a.data.date));
+  blog = blog.slice(0, 10);
 
-	return rss({
-		title: sitedata.title,
-		description: sitedata.description,
-		site: sitedata.url,
-		items: blog.map((post) => ({
-			title: post.data.title,
-			pubDate: post.data.pubDate,
-			description: post.data.description,
-			// Compute RSS link from post `id`
-			// This example assumes all posts are rendered as `/blog/[id]` routes
-			link: `/blog/${post.id}/`,
-		})),
-	});
+  // get the full URL for the stylesheet
+  // const fullUrl = new URL(context.request.url);
+  // const currentOrigin = fullUrl.origin;
+  // const stylesheetURL = new URL('/feeds/feed.xslt', currentOrigin).href;
+  // const stylesheetURL = 'https://raw.githubusercontent.com/genmon/aboutfeeds/refs/heads/main/tools/pretty-feed-v3.xsl';
+
+  return rss({
+    title: sitedata.title,
+    description: sitedata.description,
+    site: context.site,
+    // stylesheet: stylesheetURL,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.date,
+      description: post.data.description,
+      link: `/blog/${post.slug}/`,
+      categories: post.data.tags || [],
+      // commentsUrl: post.data.commentsUrl || null,
+      // enclosure: {
+      //   url: '/media/alpha-centauri.aac',
+      //   length: 124568,
+      //   type: 'audio/aac',
+      // },
+    })),
+  });
 }

051eac275b02fb855c8ee1ac58bfca6a4b5b95c7
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2025-06-19 10:21:40 +0700

    build(netlify): add netlify edge function configuration and setup

---
+++ netlify/edge-functions/geolocation.ts
@@ -0,0 +1,16 @@
+import type { Context } from '@netlify/edge-functions';
+
+export default async (_request: Request, context: Context) => {
+  // Get the page content
+  const response = await context.next();
+  const page = await response.text();
+
+  // Search for the placeholder
+  const regex = /COUNTRYNAME/i;
+
+  // Replace the content
+  const countryName = context.geo?.country?.name || 'somewhere in the world';
+
+  const updatedPage = page.replace(regex, countryName);
+  return new Response(updatedPage, response);
+};

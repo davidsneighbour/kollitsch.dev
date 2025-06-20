8c7164ea49c0a8cf96ed271d6fe6468ff992890c
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2025-06-20 08:43:37 +0700

    guild: refactor release script locations

--- .versionrc.cjs
+++ .versionrc.cjs
@@ -15,18 +15,18 @@ try {
 }
 
 const standardVersion = {
-  scripts: {
-    prerelease: "./scripts/prerelease",
-  },
-  releaseCount: 1,
   bumpFiles: [{ filename: "package.json", type: "json" }],
 
   // for available options in the conventional changelog configuration spec see
   // https://github.com/conventional-changelog/conventional-changelog-config-spec/blob/master/versions/2.2.0/README.md
   header: "Changelog",
+  releaseCount: 1,
+  scripts: {
+    prerelease: "./src/scripts/prerelease",
+  },
   types: shared.map(({ type, header }) => ({
-    type,
     section: header,
+    type,
   })),
 };
 

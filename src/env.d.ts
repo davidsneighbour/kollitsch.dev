c9fc85b6e2343c867a4965d6ae29bc69d9a75736
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2025-06-20 07:41:43 +0700

    build: add AlpineJS to env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@
 // @todo check why this needs to be ignored
 // biome-ignore lint/correctness/noUnusedVariables: this is used for type definitions
 interface Window {
+  Alpine: import('alpinejs').Alpine;
   theme: {
     setTheme: (theme: 'auto' | 'dark' | 'light') => void;
     getTheme: () => 'auto' | 'dark' | 'light';

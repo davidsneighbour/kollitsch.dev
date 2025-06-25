a2e4bb29f55f4a0fe804de57f05ba2dcd0411bce
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2025-06-26 05:39:50 +0700

    theme(fix): typecasting for meta tags

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,22 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
+/**
+ * Defines the content type for the post property. Use to extend the property
+ * evaluation:
+ *
+ * @example
+ * ```ts
+ * interface Props extends PostProp<'blog'> {
+ * somethingElse: string;
+ * }
+ * let { post, somethingElse }: Props = Astro.props;
+ * ```
+ */
+type PostProp<TCollection extends string> = {
+  post: CollectionEntry<TCollection>;
+};
+
 // @todo check why this needs to be ignored
 // biome-ignore lint/correctness/noUnusedVariables: this is used for type definitions
 interface Window {
@@ -12,3 +28,15 @@ interface Window {
     getDefaultTheme: () => 'auto' | 'dark' | 'light';
   };
 }
+
+/**
+ * MetaConfig interface defines the structure for meta tags configuration.
+ */
+type MetaConfig = {
+  name: Record<string, string>;
+  httpEquiv: Record<string, string>;
+  link: Array<{
+    rel: string;
+    href: string;
+  }>;
+};

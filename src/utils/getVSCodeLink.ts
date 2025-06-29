fbc6dc09cfec8362a9e4d895e6e1b969915c6711
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2025-06-29 16:30:35 +0700

    theme(fix): refactor VSCode links generation

--- src/utils/getVSCodeLink.ts
+++ src/utils/getVSCodeLink.ts
@@ -13,3 +13,24 @@ export function getVSCodeLink(relativePath: string): string {
   const fullPath = projectRoot + relativePath;
   return `vscode://file/${fullPath}`;
 }
+
+/**
+ * Returns a `vscode://file/` URL to a given file path inside your project.
+ *
+ * @param relativePath - Path relative to the project root, e.g. 'src/components/meta/PublishData.astro'
+ * @returns VS Code deep link to open the file.
+ */
+export function getVSCodeURL(relativePath: string): string {
+  const currentFile = fileURLToPath(import.meta.url);
+  const currentDir = dirname(currentFile);
+  const projectRoot = resolve(currentDir, '../../'); // Adjust this if file moves
+  const fullPath = projectRoot + relativePath;
+  return `vscode://file/${fullPath}`;
+}
+
+export function getVSCodeUrlById(id: string, type: 'blog' = 'blog'): string {
+  const vscodeURL = getVSCodeURL(
+    '/src/content/' + type + '/' + id + '/index.md',
+  );
+  return vscodeURL;
+}

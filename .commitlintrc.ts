5005dce81f83851dc1fe9dca8b9564cc427c248a
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2025-07-12 06:48:10 +0700

    build: add vscode as scope to commitlint config

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -9,71 +9,71 @@ if (!Array.isArray(shared) || shared.length === 0) {
 
 // cz-git prompt messages
 const promptMessages: CommitizenGitOptions['messages'] = {
-  type: "Select the type of change that you're committing:",
-  scope: 'Denote the SCOPE of this change (optional):',
-  customScope: 'Denote the SCOPE of this change:',
-  subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
   body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
   breaking:
     'List any BREAKING CHANGES (optional). Use "|" to break new line:\n',
+  confirmCommit: 'Are you sure you want to proceed with the commit above?',
+  customFooterPrefixs: 'Input ISSUES prefix:',
+  customScope: 'Denote the SCOPE of this change:',
+  footer: 'List any ISSUES by this change. E.g.: #31, user/repo#32, #34:\n',
   footerPrefixsSelect:
     'Select the ISSUES type of changeList by this change (optional):',
-  customFooterPrefixs: 'Input ISSUES prefix:',
-  footer: 'List any ISSUES by this change. E.g.: #31, user/repo#32, #34:\n',
-  confirmCommit: 'Are you sure you want to proceed with the commit above?',
+  scope: 'Denote the SCOPE of this change (optional):',
+  subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
+  type: "Select the type of change that you're committing:",
 };
 
 // cz-git types array
 const czTypes = shared.map(({ type, description }) => ({
-  value: type,
   name: `${type}: ${description}`,
+  value: type,
 }));
 
 const commitlintConfig: UserConfig = {
-  rules: {
-    // overriding commitlint rules, e.g.:
-    // 'header-max-length': [2, 'always', 72],
-    // @see https://commitlint.js.org/reference/rules.html
-  },
   prompt: {
-    messages: promptMessages,
-    types: czTypes,
-    useEmoji: false,
-    themeColorCode: '38;5;159',
-    customScopesAlign: 'bottom-top',
-    scopes: [],
-    allowCustomScopes: true,
-    allowEmptyScopes: true,
-    scopeOverrides: {
-      theme: ['fix', 'feat'],
-      build: ['deps', 'deps-dev'],
-      content: ['new', 'fix', 'schema', 'update'],
-    },
-    customScopesAlias: 'custom',
-    emptyScopesAlias: 'empty',
-    upperCaseSubject: false,
-    markBreakingChangeMode: true,
     allowBreakingChanges: ['feat', 'fix'],
-    breaklineNumber: 100,
-    breaklineChar: '|',
-    skipQuestions: [],
-    issuePrefixs: [
-      { value: 'closed', name: 'closed:   ISSUE has been resolved' },
-      { value: 'see', name: 'see:      ISSUE is connected to this commit' },
-    ],
-    customIssuePrefixsAlign: 'top',
-    emptyIssuePrefixsAlias: 'skip',
-    customIssuePrefixsAlias: 'custom',
     allowCustomIssuePrefixs: true,
+    allowCustomScopes: true,
     allowEmptyIssuePrefixs: true,
+    allowEmptyScopes: true,
+    breaklineChar: '|',
+    breaklineNumber: 100,
     confirmColorize: true,
-    maxHeaderLength: Infinity,
-    maxSubjectLength: 50,
-    minSubjectLength: 0,
+    customIssuePrefixsAlias: 'custom',
+    customIssuePrefixsAlign: 'top',
+    customScopesAlias: 'custom',
+    customScopesAlign: 'bottom-top',
     defaultBody: '',
     defaultIssues: '',
     defaultScope: '',
     defaultSubject: '',
+    emptyIssuePrefixsAlias: 'skip',
+    emptyScopesAlias: 'empty',
+    issuePrefixs: [
+      { name: 'closed:   ISSUE has been resolved', value: 'closed' },
+      { name: 'see:      ISSUE is connected to this commit', value: 'see' },
+    ],
+    markBreakingChangeMode: true,
+    maxHeaderLength: Infinity,
+    maxSubjectLength: 50,
+    messages: promptMessages,
+    minSubjectLength: 0,
+    scopeOverrides: {
+      build: ['deps', 'deps-dev', 'vscode'],
+      content: ['new', 'fix', 'schema', 'update'],
+      theme: ['fix', 'feat'],
+    },
+    scopes: [],
+    skipQuestions: [],
+    themeColorCode: '38;5;159',
+    types: czTypes,
+    upperCaseSubject: false,
+    useEmoji: false,
+  },
+  rules: {
+    // overriding commitlint rules, e.g.:
+    // 'header-max-length': [2, 'always', 72],
+    // @see https://commitlint.js.org/reference/rules.html
   },
 };
 

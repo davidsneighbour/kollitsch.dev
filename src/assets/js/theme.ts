c3c93adeb8d25391e8b02273bc86fe5b4fd909fc
Patrick Kollitsch <davidsneighbourdev@gmail.com>
2025-09-12 22:07:30 +0700

    theme(fix): proper implementation for the footer greeting

--- src/assets/js/theme.ts
+++ src/assets/js/theme.ts
@@ -1,1 +1,9 @@
 import './syncstring.js';
+import setup from '@data/setup.json' with { type: 'json' };
+import { renderIntroduction } from './render-intro.js';
+
+const target = document.getElementById('about-introduction');
+if (target) {
+  const html = renderIntroduction(setup.introduction, setup.greetings);
+  target.innerHTML = html;
+}

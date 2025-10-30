072d0911eae07ab911d6a2ea5ebc83cd5aab22f7
Patrick Kollitsch <83281+davidsneighbour@users.noreply.github.com>
2025-10-31 05:20:51 +0700

    theme(fix): design changes on code blocks

--- astro.config.ts
+++ astro.config.ts
@@ -9,14 +9,13 @@ import beep from '@rollup/plugin-beep';
 import yaml from '@rollup/plugin-yaml';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
-import expressiveCode from 'astro-expressive-code';
+import expressiveCode, { createInlineSvgUrl } from 'astro-expressive-code';
 import icon from 'astro-icon';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import pagefind from './src/scripts/integrations/pagefind.ts';
 import { createLogger } from './src/utils/logger.ts';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 const crontabTmLanguage = JSON.parse(
   fs.readFileSync('./src/config/tmLanguages/crontab.tmLanguage.json', 'utf-8'),
 );
@@ -109,8 +108,22 @@ export default defineConfig({
       },
     }),
     expressiveCode({
-      shiki: { langs: [crontabTmLanguage] },
-      styleOverrides: { frames: { frameBoxShadowCssValue: '0' } },
+      shiki: {
+        langs: [
+          crontabTmLanguage
+        ],
+        // bundledLangs: ['astro', 'sass'],
+      },
+      styleOverrides: {
+        codeFontFamily: "'jetbrainsmono', monospace",
+        codeFontWeight: '300',
+        uiFontFamily: "'jetbrainsmono', monospace",
+        uiFontWeight: '300',
+        frames: {
+          frameBoxShadowCssValue: '0',
+          copyIcon: createInlineSvgUrl('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-code-slash" viewBox="0 0 16 16"> <path d = "M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0m6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0" /></svg>'),
+        }
+      },
       themeCssSelector: (theme) => `[data-code-theme='${theme.name}']`,
       themes: ['dracula', 'light-plus'],
       useDarkModeMediaQuery: false,

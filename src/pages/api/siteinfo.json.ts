d504407aa4993cd5868cb7012fa788af489eafb7
Patrick Kollitsch <83281+davidsneighbour@users.noreply.github.com>
2025-11-01 06:56:48 +0700

    theme(fix): make api call static

--- src/pages/api/siteinfo.json.ts
+++ src/pages/api/siteinfo.json.ts
@@ -1,3 +1,6 @@
+// this is static and has no parameters and can be prerendered
+export const prerender = true;
+
 import type { APIContext } from 'astro';
 
 export function GET({ generator, site }: APIContext) {

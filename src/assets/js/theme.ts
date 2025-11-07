b91e7a78b91c803f2fb6833a6f488d6a5ac5c17b
Patrick Kollitsch <83281+davidsneighbour@users.noreply.github.com>
2025-11-07 20:42:16 +0700

    feat: add lenis scroll animation

--- src/assets/js/theme.ts
+++ src/assets/js/theme.ts
@@ -1,9 +1,22 @@
 import './syncstring.ts';
 import setup from '@data/setup.json' with { type: 'json' };
 import { renderIntroduction } from './render-intro.ts';
+import Lenis from 'lenis'
 
 const target = document.getElementById('about-introduction');
 if (target) {
   const html = renderIntroduction(setup.introduction, setup.greetings);
   target.innerHTML = html;
 }
+
+// const lenis = new Lenis({
+new Lenis({
+  autoRaf: true,
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // easeOutExpo
+});
+
+// Listen for the scroll event and log the event data
+// lenis.on('scroll', (e) => {
+//   // console.log(e);
+// });

3416fb0ddf5db71446da059a86e9fff7f142c785
Patrick Kollitsch <83281+davidsneighbour@users.noreply.github.com>
2025-11-07 20:47:51 +0700

    theme(fix): remove comments

--- src/assets/js/theme.ts
+++ src/assets/js/theme.ts
@@ -9,14 +9,8 @@ if (target) {
   target.innerHTML = html;
 }
 
-// const lenis = new Lenis({
 new Lenis({
   autoRaf: true,
   duration: 1.2,
   easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // easeOutExpo
 });
-
-// Listen for the scroll event and log the event data
-// lenis.on('scroll', (e) => {
-//   // console.log(e);
-// });

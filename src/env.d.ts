6caa6be0cc7887754bdb461fc874d5817af03d86
Patrick Kollitsch <83281+davidsneighbour@users.noreply.github.com>
2025-11-12 13:23:33 +0700

    refactor: move specific types out of env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,4 @@
-// @todo move into their original files as type exports
-
-/**
- * Adding global types to the Window object.
- */
+/** Adding global types to the Window object. */
 interface Window {
   theme: {
     setTheme: (theme: 'auto' | 'dark' | 'light') => void;
@@ -11,52 +7,3 @@ interface Window {
     getDefaultTheme: () => 'auto' | 'dark' | 'light';
   };
 }
-
-/**
- * Defines the content type for the post property. Use to extend the property
- * evaluation:
- *
- * @example
- * ```ts
- * interface Props extends PostProp<'blog'> {
- *   anotherProperty: string;
- * }
- * const { post, anotherProperty }: Props = Astro.props;
- * ```
- */
-type PostProp<TCollection extends string> = {
-  post: CollectionEntry<TCollection>;
-};
-
-/**
- * MetaConfig interface defines the structure for meta tags configuration.
- * @see data/meta.json
- */
-type MetaConfig = {
-  name: Record<string, string>;
-  httpEquiv: Record<string, string>;
-  link: Array<{
-    rel: string;
-    href: string;
-  }>;
-};
-
-/**
- * Navigation item type definition.
- * Represents a single navigation item with properties for label, link, icon,
- * and optional matching criteria.
- */
-type NavItem = {
-  label: string;
-  link: string;
-  icon: IconName;
-  match?: 'full' | 'prefix';
-  matchPaths?: string[];
-  children?: NavItem[];
-};
-
-/**
- * Navigation type definition.
- * Represents an array of navigation items.
- */
-type Navigation = NavItem[];
--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,3 +1,25 @@
+import type { IconName } from '@utils/icon-names';
+
+/**
+ * Navigation type definition.
+ * Represents an array of navigation items.
+ */
+export type Navigation = NavItem[];
+
+/**
+ * Navigation item type definition.
+ * Represents a single navigation item with properties for label, link, icon,
+ * and optional matching criteria.
+ */
+export type NavItem = {
+  label: string;
+  link: string;
+  icon: IconName;
+  match?: 'full' | 'prefix';
+  matchPaths?: string[];
+  children?: NavItem[];
+};
+
 export function isActive(item: NavItem, pathname: string): boolean {
   const matchType = item.match ?? 'full';
   const directMatch =
